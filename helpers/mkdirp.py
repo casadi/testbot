be8d9cff2939311327553d5016a4fafd7d197724
Your Name <you@example.com>
2013-10-05 20:28:41 +0200

    win64 commit

---
+++ helpers/mkdirp.py
@@ -0,0 +1,14 @@
+import sys
+import os, errno
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+mkdir_p(sys.argv[1])
+
+        

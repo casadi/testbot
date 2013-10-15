770d3f7e0eb8eb2c64c3c3f0f863628e7b7c40e1
Joris Gillis <joris.gillis42@gmail.com>
2013-10-15 13:10:46 +0200

    error

--- helpers/extra_install.py
+++ helpers/extra_install.py
@@ -8,4 +8,4 @@ d = out.split(" ")[0][2:]
 
 f = file("extra_install.cmake","w")
 for i in glob(d+"/*.a"):
-  f.write("install(FILES %s DESTINATION ipopt/dir)\n" % i)
+  f.write("install(FILES %s DESTINATION lib/ipopt)\n" % i)

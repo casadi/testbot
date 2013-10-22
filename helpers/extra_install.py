40c2d8348bf567d7c195a1224e4c8f6d7509b51e
Joris Gillis <joris.gillis42@gmail.com>
2013-10-22 16:12:01 +0200

    work

--- helpers/extra_install.py
+++ helpers/extra_install.py
@@ -1,5 +1,6 @@
 import subprocess
 from glob import glob
+import os
 
 p = subprocess.Popen(["pkg-config","--libs-only-L","ipopt"],stdout=subprocess.PIPE)
 (out,err) = p.communicate()
@@ -8,7 +9,7 @@ d = out.split(" ")[0][2:]
 
 f = file("extra_install.cmake","w")
 for i in glob(d+"/*.a"):
-  ri = "libcasadi_"+i[3:]
+  ri = "libcasadi_"+os.path.basename(i)[3:]
   f.write("install(FILES %s DESTINATION lib RENAME %s)\n" % (i,ri))
   
 f.write("install(FILES ${PROJECT_BINARY_DIR}/swig/CasadiTree.hs ${PROJECT_BINARY_DIR}/swig/CasadiClasses.hs ${PROJECT_BINARY_DIR}/swig/swiginclude.hpp ${PROJECT_BINARY_DIR}/swig/linklist.txt DESTINATION share/casadi)\n")

55086184e0b9bac80708abf6f5e7a13975d003be
Joris Gillis <joris.gillis42@gmail.com>
2014-04-15 08:38:33 +0200

    remove extras

--- helpers/extra_install.py
+++ helpers/extra_install.py
@@ -8,10 +8,10 @@ p = subprocess.Popen(["pkg-config","--libs-only-L","ipopt"],stdout=subprocess.PI
 d = out.split(" ")[0][2:]
 
 f = file("extra_install.cmake","w")
-for i in glob(d+"/*.a"):
-  ri = "libcasadi_"+os.path.basename(i)[3:]
-  f.write("install(FILES %s DESTINATION lib RENAME %s)\n" % (i,ri))
+#for i in glob(d+"/*.a"):
+#  ri = "libcasadi_"+os.path.basename(i)[3:]
+#  f.write("install(FILES %s DESTINATION lib RENAME %s)\n" % (i,ri))
   
-f.write("install(FILES ${PROJECT_BINARY_DIR}/swig/CasadiTree.hs ${PROJECT_BINARY_DIR}/swig/CasadiClasses.hs ${PROJECT_BINARY_DIR}/swig/swiginclude.hpp ${PROJECT_BINARY_DIR}/swig/linklist.txt DESTINATION share/casadi)\n")
+#f.write("install(FILES ${PROJECT_BINARY_DIR}/swig/CasadiTree.hs ${PROJECT_BINARY_DIR}/swig/CasadiClasses.hs ${PROJECT_BINARY_DIR}/swig/swiginclude.hpp ${PROJECT_BINARY_DIR}/swig/linklist.txt DESTINATION share/casadi)\n")
 
 f.write("""install(DIRECTORY ${PROJECT_SOURCE_DIR}/swig DESTINATION share/casadi FILES_MATCHING PATTERN "*.i" PATTERN "*doc.i" EXCLUDE)\n""")

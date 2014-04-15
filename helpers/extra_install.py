0a7fe1c2d45ed5419ef149b5ccd6cc70b5bcda3d
Joris Gillis <joris.gillis42@gmail.com>
2014-04-15 08:39:54 +0200

    remove extras

--- helpers/extra_install.py
+++ helpers/extra_install.py
@@ -14,4 +14,4 @@ f = file("extra_install.cmake","w")
   
 #f.write("install(FILES ${PROJECT_BINARY_DIR}/swig/CasadiTree.hs ${PROJECT_BINARY_DIR}/swig/CasadiClasses.hs ${PROJECT_BINARY_DIR}/swig/swiginclude.hpp ${PROJECT_BINARY_DIR}/swig/linklist.txt DESTINATION share/casadi)\n")
 
-f.write("""install(DIRECTORY ${PROJECT_SOURCE_DIR}/swig DESTINATION share/casadi FILES_MATCHING PATTERN "*.i" PATTERN "*doc.i" EXCLUDE)\n""")
+#f.write("""install(DIRECTORY ${PROJECT_SOURCE_DIR}/swig DESTINATION share/casadi FILES_MATCHING PATTERN "*.i" PATTERN "*doc.i" EXCLUDE)\n""")

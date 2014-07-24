80f7c08069042ab8367f2c0ec0022bc7df6f3a66
casadibot <casaditestbot@gmail.com>
2014-07-24 13:42:34 +0200

    minimal now works on 14.04

--- helpers/publish_builds.py
+++ helpers/publish_builds.py
@@ -4,6 +4,8 @@ from shutil import copy, copyfile
 from os.path import join, split
 from distro import *
 
+import shutil
+
 #import sys
 #sys.exit(0)
 
@@ -41,11 +43,18 @@ def copy_files(src_glob, dst_folder):
             print str(e)
             pass
 
-#copy_files("..\\..\\libraries\\*.dll","python\\casadi")
+#copy_files("python_install/lib/*.so","python_install/casadi")
 
 # Clean dist dir
 for i in glob("python_install/dist/*"):
     os.remove(i)
+
+for i in glob("python_install/lib/*.so"):
+    if os.path.islink(i):
+	ir = os.path.realpath(i)
+	os.unlink(i)       
+	shutil.copyfile(ir,i)
+
 f = file('python_install/setup.py','w')
 f.write("""
 from distutils.core import setup, Extension
@@ -59,7 +68,8 @@ setup(name="python-casadi",
     author="Joel Andersson",
     url="casadi.org",
     packages=["casadi","casadi.tools","casadi.tools.graph"],
-    package_data={"casadi": ["*.so","../lib/*.so"]}
+    package_data={"casadi": ["*.so"]},
+    data_files=[('/usr/lib',glob("lib/*.so"))]
 )
 
 """ % release)

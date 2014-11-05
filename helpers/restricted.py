27446176df49f576426b84eb3a4dd69dc8990e74
Joris Gillis <joris.gillis42@gmail.com>
2014-11-05 20:48:44 +0100

    download script

--- helpers/restricted.py
+++ helpers/restricted.py
@@ -34,8 +34,25 @@ def upload(filename):
      assert r.ok, str(r)
      time.sleep(1)
         
+
   rs = s.post(release["upload_url"].replace("{?name}",""),params={"name": filename,"label": ""},data=file(filename,"r"),verify=False,headers={"Content-Type":"application/gzip"},timeout=timeout)
   assert rs.ok, str(rs.json())
   return rs
-  
+    
+def download(filename):
+  r = s.get('https://api.github.com/repos/jgillis/restricted/releases',timeout=timeout)
+  l = filter(lambda x: x["name"]=="perpetual",r.json())
+  release = l[0]
+
+  assets = s.get('https://api.github.com/repos/jgillis/releases/%d/assets' % release["id"],timeout=timeout)
+  assert(assets.ok)
+  time.sleep(1)
+
+  for a in assets.json():
+    if a["name"]==filename:
+      rs = s.get(a["url"], stream=True,verify=False,headers={"Content-Type":"application/octet-stream"},timeout=timeout)
+      assert rs.ok, str(rs.json())
+      with open(filename, 'wb') as f:
+          for chunk in r.iter_content():
+              f.write(chunk)
   

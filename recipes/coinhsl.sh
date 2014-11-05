417c9faeb0abd03a4cdc53a0f6c3657d61ead7a4
jgillis <joris.gillis42@gmail.com>
2014-11-05 16:51:40 +0100

    Update coinhsl.sh

--- recipes/coinhsl.sh
+++ recipes/coinhsl.sh
@@ -4,6 +4,6 @@ sudo pip install requests
 mypwd=`pwd`
 #pushd restricted && wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz && tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz && ./configure --prefix=$mypwd/coinhsl-install LIBS="-llapack" --with-blas="-L/usr/lib -lblas" CXXFLAGS="-g -O2 -fopenmp" FCFLAGS="-g -O2 -fopenmp" && make && make install && cd $mypwd/coinhsl-install/lib && ln -s libcoinhsl.so libhsl.so && popd && tar -zcvf libhsl.tar.gz $mypwd/coinhsl-install/lib 
 
-touch libhsl.tar.gz
+echo "test" > libhsl.tar.gz
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('libhsl.tar.gz')"
 

96e889148f6c26dd49431805b8a00e480d51edea
jgillis <joris.gillis42@gmail.com>
2014-11-06 09:15:04 +0100

    Update snopt7.sh

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get install -y libblas-dev liblapack-dev gfortran f2c
-sudo add-apt-repository -y ppa:costamagnagianfranco/autoconf
-sudo apt-get update -q
-sudo apt-get install -y autoconf2.69
+#sudo add-apt-repository -y ppa:costamagnagianfranco/autoconf
+#sudo apt-get update -q
+#sudo apt-get install -y autoconf2.69
 mypwd=`pwd`
 mkdir "$mypwd/snopt7-install"
 pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" && make && make install && popd
-git clone https://github.com/snopt/snopt-interface.git
+git clone https://github.com/jgillis/snopt-interface.git
 pushd snopt-interface && ./autogen.sh && ./configure --with-snopt="$mypwd/snopt7-install/lib" --prefix="$mypwd/snopt7-install" && make && make install && popd
 tar -zcvf snopt7.tar.gz -C snopt7-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('snopt7.tar.gz')"

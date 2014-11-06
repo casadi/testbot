f776d25982e3a6c0a2766951d5aed53b9bc5c15b
jgillis <joris.gillis42@gmail.com>
2014-11-06 09:27:10 +0100

    Update snopt7.sh

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -5,7 +5,8 @@ tar -xvf autoconf-2.69.tar.gz
 pushd autoconf-2.69 && ./configure && make && sudo make install && popd
 mypwd=`pwd`
 mkdir "$mypwd/snopt7-install"
-pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" && make && make install && popd
+pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" --with-c && make && make install && popd
+ls -al "$mypwd/snopt7-install/lib"
 git clone https://github.com/snopt/snopt-interface.git
 pushd snopt-interface && ./autogen.sh && ./configure --with-snopt="$mypwd/snopt7-install/lib" --prefix="$mypwd/snopt7-install" && make && make install && popd
 tar -zcvf snopt7.tar.gz -C snopt7-install .

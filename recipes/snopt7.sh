0cebd5cc89416e39f0efd19a069f2a9871058158
jgillis <joris.gillis42@gmail.com>
2014-11-06 08:10:18 +0100

    Update snopt7.sh

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get install -y libblas-dev liblapack-dev gfortran
 mypwd=`pwd`
+mkdir "$mypwd/snopt7-install"
 pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" && make && make install && popd && tar -zcvf snopt7.tar.gz -C snopt7-install .
 
+pwd
+ls
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('snopt7.tar.gz')"

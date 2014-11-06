55f96d462a5c25d6b65167b68587549d61ec947c
jgillis <joris.gillis42@gmail.com>
2014-11-06 08:19:48 +0100

    Update snopt7.sh

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get install -y libblas-dev liblapack-dev gfortran
+sudo apt-get install -y libblas-dev liblapack-dev gfortran f2c
 mypwd=`pwd`
 mkdir "$mypwd/snopt7-install"
-pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" --with-api && make && make install && popd && tar -zcvf snopt7.tar.gz -C snopt7-install .
+pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" --with-api --with-c && make && make install && popd && tar -zcvf snopt7.tar.gz -C snopt7-install .
 
 pwd
 ls

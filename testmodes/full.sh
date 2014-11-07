dcb5251c5847356f90300774f3293fea846992dc
jgillis <joris.gillis42@gmail.com>
2014-11-07 15:50:56 +0100

    Update full.sh

--- testmodes/full.sh
+++ testmodes/full.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get install python-lxml -y
 git clone https://github.com/jgillis/pyreport.git
+nm $SNOPT/lib/libsnopt7.so | grep snlog2_
 pushd pyreport && sudo python setup.py install && popd
 python -c "from casadi.tools import *;loadAllCompiledPlugins()"
 pushd test & make trunktesterbot MEMCHECK=-memcheck & popd

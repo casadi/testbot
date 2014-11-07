eca494898e1a7efa4185f2e3539b530781941492
jgillis <joris.gillis42@gmail.com>
2014-11-07 18:07:03 +0100

    Update minimal.sh

--- testmodes/minimal.sh
+++ testmodes/minimal.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+mkdir build
+pushd build
+cmake -DWITH_WORHP=ON -DWITH_SLICOT=ON ..
+make
+sudo make install
+popd
 python -c "from casadi.tools import *;loadAllCompiledPlugins()"
 pushd test && make unittests_py examples_code_py && popd

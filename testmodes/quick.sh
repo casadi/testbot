ed3056f4d884698a811307b41c264375810d7638
jgillis <joris.gillis42@gmail.com>
2014-11-07 14:19:45 +0100

    Update quick.sh

--- testmodes/quick.sh
+++ testmodes/quick.sh
@@ -14,4 +14,6 @@ pushd build && export languagetool="$mypwd/LanguageTool-2.7" make spell && popd
 pushd misc && python autogencode.py && popd
 sh /home/travis/build/testbot/helpers/acommit.sh "automatic code generation"
 git pull
+git config --global push.default simple
+git remote set-url origin git@github.com:casadi/casadi.git
 ssh-agent bash -c 'ssh-add /home/travis/build/testbot/id_rsa_travis; git push'

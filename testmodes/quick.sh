763efcbb76408c7991f8a5332f496c5ef9d86238
Joris Gillis <joris.gillis42@gmail.com>
2014-11-07 14:57:30 +0100

    quick update

--- testmodes/quick.sh
+++ testmodes/quick.sh
@@ -16,4 +16,4 @@ sh /home/travis/build/testbot/helpers/acommit.sh "automatic code generation"
 git pull
 git config --global push.default simple
 git remote set-url origin git@github.com:casadi/casadi.git
-ssh-agent bash -c 'ssh-add /home/travis/build/testbot/id_rsa_travis; git push'
+ssh-agent bash -c 'ssh-add /home/travis/build/testbot/id_rsa_travis; git push origin HEAD:$TRAVIS_BRANCH'

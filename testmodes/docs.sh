8a7f4fdb4f476e86545fb4f1a138df998eaef7fe
Joris Gillis <joris.gillis42@gmail.com>
2014-11-07 21:26:43 +0100

    split up

--- testmodes/docs.sh
+++ testmodes/docs.sh
@@ -11,8 +11,13 @@ git clone https://github.com/jgillis/pyreport.git
 pushd pyreport && sudo python setup.py install && popd
 sudo apt-get install python-lxml python-docutils texlive-science valgrind -y
 
-python -c "from casadi.tools import *;loadAllCompiledPlugins()"
-pushd test && make trunktesterbot MEMCHECK=-memcheck && popd
-pushd build && make json && popd
+pushd docs/api && make full && popd
+
+if [[ $TRAVIS_BRANCH == 'develop' ]]; then
+    pushd docs/api && python /home/travis/build/testbot/helpers/publish_doc.py && popd
+    git pull
+    sh /home/travis/build/testbot/helpers/acommit.sh "automatic documentation update"
+    python /home/travis/build/testbot/helpers/publish_builds.py
+fi
 
 

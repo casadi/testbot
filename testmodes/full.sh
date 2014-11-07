b3b15d2c4cebba3ae7da4b71f057ca02bf610492
jgillis <joris.gillis42@gmail.com>
2014-11-07 18:06:12 +0100

    Update full.sh

--- testmodes/full.sh
+++ testmodes/full.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+mkdir build
+pushd build
+cmake -DWITH_WORHP=ON -DWITH_SLICOT=ON -DWITH_OOQP=ON -DWITH_PROFILING=ON -DWITH_DOC=ON -DWITH_EXAMPLES=ON -DWITH_COVERAGE=ON -DWITH_WERROR=ON -DWITH_EXTRA_WARNINGS=ON .. 
+make
+sudo make install
 sudo apt-get install python-lxml python-docutils texlive-science valgrind -y
+popd
 git clone https://github.com/jgillis/pyreport.git
 nm $SNOPT/lib/libsnopt7.so | grep snlog2_
 pushd pyreport && sudo python setup.py install && popd

f3f4d5c26082af3fc1e99c248cbd2da3bc653137
jgillis <joris.gillis42@gmail.com>
2014-11-13 14:52:17 +0100

    Update win_msys.bat

--- testmodes/win_msys.bat
+++ testmodes/win_msys.bat
@@ -3,6 +3,6 @@ mkdir my_build_msys
 cd my_build_msys
 cmake "-GMSYS Makefiles" ..
 make
-make install
+make install install_python
 python -c "from casadi.tools import *;loadAllCompiledPlugins()"
 cd ../test; make unittests_py examples_code_py; cd ../build

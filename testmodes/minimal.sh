f94d66ccd71e499cce7d342d617296c954ad7c88
Joris Gillis <joris.gillis42@gmail.com>
2014-11-28 09:15:58 +0100

    WITH_PYTHON, WITH_JSON

--- testmodes/minimal.sh
+++ testmodes/minimal.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir build
 pushd build
-cmake -DWITH_WORHP=ON -DWITH_SLICOT=ON ..
+cmake -DWITH_WORHP=ON -DWITH_SLICOT=ON -DWITH_PYTHON=ON -DWITH_JSON=ON  ..
 make
 sudo make install
 popd

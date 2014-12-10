c7e190b0b41ffc5ba9d08ba2682f78160e64e463
jgillis <joris.gillis42@gmail.com>
2014-12-10 09:31:36 +0100

    Create swig.sh

---
+++ recipes/swig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+sudo apt-get install -y libpcre3-dev
+mypwd=`pwd`
+pushd restricted && wget http://sourceforge.net/projects/swig/files/swig/swig-2.0.11/swig-2.0.11.tar.gz && tar -xvf swig-2.0.11.tar.gz
+pushd swig-2.0.11 && dh_make --s --copyright gpl -f ../swig-2.0.11.tar.gz && dpkg-buildpackage -rfakeroot
+tree
+#export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && travis_retry python -c "from restricted import *; upload('libhsl.tar.gz')"

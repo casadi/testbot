6ca0950e0da64732ef91705254f996afe3406247
jgillis <joris.gillis42@gmail.com>
2014-12-10 10:36:34 +0100

    Update swig.sh

--- recipes/swig.sh
+++ recipes/swig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt-get install -y libpcre3-dev
+sudo apt-get install -y libpcre3-dev tree dh-make
 mypwd=`pwd`
 pushd restricted && wget http://sourceforge.net/projects/swig/files/swig/swig-2.0.11/swig-2.0.11.tar.gz && tar -xvf swig-2.0.11.tar.gz >/dev/null
 pushd swig-2.0.11 && dh_make --s --copyright gpl -f ../swig-2.0.11.tar.gz && dpkg-buildpackage -rfakeroot

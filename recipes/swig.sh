1834649c8478f95f4b6e11a61c68e6e176202ed5
jgillis <joris.gillis42@gmail.com>
2014-12-10 10:57:04 +0100

    Update swig.sh

--- recipes/swig.sh
+++ recipes/swig.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt-get install -y libpcre3-dev
 mypwd=`pwd`
 pushd restricted && wget http://sourceforge.net/projects/swig/files/swig/swig-2.0.11/swig-2.0.11.tar.gz && tar -xvf swig-2.0.11.tar.gz >/dev/null
-pushd swig-2.0.11 && ./configure --prefix=$mypwd/swig-install .. && make && make install
+pushd swig-2.0.11 && ./configure --prefix=$mypwd/swig-install && make && make install
 popd && popd
 tar -zcvf swig.tar.gz -C swig-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig.tar.gz')"

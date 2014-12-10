bd5d0d900959543ff4767b5d21f8052494fa5d37
jgillis <joris.gillis42@gmail.com>
2014-12-10 10:28:55 +0100

    Update snopt7.sh

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -6,8 +6,8 @@ wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 tar -xvf autoconf-2.69.tar.gz
 pushd autoconf-2.69 && ./configure && make && sudo make install && popd
 mypwd=`pwd`
-sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
-sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
+#sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
+#sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 mkdir "$mypwd/snopt7-install"
 pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" && make && make install && popd
 ls -al "$mypwd/snopt7-install/lib"

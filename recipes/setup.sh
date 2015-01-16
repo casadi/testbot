d8313c06a513351756c5e3fb189db0281fb6d7b5
jgillis <joris.gillis42@gmail.com>
2015-01-16 13:53:15 +0100

    Update setup.sh

--- recipes/setup.sh
+++ recipes/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
 sudo pip install requests
 openssl aes-256-cbc -k "$keypass" -in id_rsa_travis.enc -out id_rsa_travis -d
 openssl aes-256-cbc -k "$keypass" -in testbotcredentials.py.enc -out testbotcredentials.py -d
 sudo chmod 600 id_rsa_travis
-ssh-agent bash -c 'ssh-add id_rsa_travis; yes | git clone git@github.com:jgillis/restricted.git'
+ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@github.com:jgillis/restricted.git'
 

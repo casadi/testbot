e3a88404b6cde5405814ea3bccfc78bee1ff179c
jgillis <joris.gillis42@gmail.com>
2015-04-20 08:07:29 +0200

    Update swig_matlab.sh

--- recipes/swig_matlab.sh
+++ recipes/swig_matlab.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt-get install -y libpcre3-dev
 mypwd=`pwd`
-pushd restricted && ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@github.com:jgillis/swig.git"
+pushd restricted && ssh-agent bash -c 'ssh-add id_rsa_travis; git clone git@github.com:jgillis/swig.git'
 pushd swig && git checkout matlab && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
 popd && popd
 tar -zcvf swig_matlab.tar.gz -C swig-matlab-install .

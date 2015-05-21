d876820c9bba0052b21626d5ae80e45ea23e44c4
jgillis <joris.gillis42@gmail.com>
2015-05-21 23:01:22 +0200

    Update publish_builds_win64.py

--- helpers/publish_builds_win64.py
+++ helpers/publish_builds_win64.py
@@ -58,9 +58,9 @@ if '+' in release:
 else:
     releasedir = release
 f.close()
-p = subprocess.Popen(["python","..\\setup.py","bdist_wininst","--target-version=2.7","--title=CasADi"],cwd="python_install")
+p = subprocess.Popen(["python","../setup.py","bdist_wininst","--target-version=2.7","--title=CasADi"],cwd="python_install")
 p.wait()
-p = subprocess.Popen(["python","..\\setup.py","bdist","--format=zip"],cwd="python_install")
+p = subprocess.Popen(["python","../setup.py","bdist","--format=zip"],cwd="python_install")
 p.wait()
 f = file('temp.batchftp','w')
 f.write("mkdir %s\n" % releasedir)

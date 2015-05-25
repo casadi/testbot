816304c635db89c9dc73f6d9331d3e6f4f96df00
jgillis <joris.gillis42@gmail.com>
2015-05-25 19:05:04 +0200

    Update mingw_ipopt.sh

--- recipes/mingw_ipopt.sh
+++ recipes/mingw_ipopt.sh
@@ -13,7 +13,7 @@ wget http://www.coin-or.org/download/source/Ipopt/Ipopt-$VERSION.tgz
 tar -xvf Ipopt-$VERSION.tgz
 pushd Ipopt-$VERSION
 pushd ThirdParty
-pushd ASL && ./get.ASL && popd
+#pushd ASL && ./get.ASL && popd
 pushd Blas && ./get.Blas && popd 
 pushd Lapack && ./get.Lapack && popd 
 pushd Metis && ./get.Metis && popd 
@@ -21,7 +21,7 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
+../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 tar -zcvf ipopt_mingw32.tar.gz -C /home/travis/ipopt_install .

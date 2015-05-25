3055f872a67d5d151eceaff57ed22d1dfe99aed1
Joris Gillis <joris.gillis42@gmail.com>
2015-05-25 17:26:12 +0200

    attempting ipopt builds

---
+++ recipes/ipopt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+VERSION=3.12.3
+
+wget http://www.coin-or.org/download/source/Ipopt/Ipopt-$VERSION.tgz
+tar -xvf Ipopt-$VERSION.tgz
+pushd Ipopt-$VERSION
+pushd ASL && ./get.ASL && popd
+pushd Blas && ./get.Blas && popd 
+pushd Lapack && ./get.Lapack && popd 
+pushd Metis && ./get.Metis && popd 
+pushd Mumps && ./get.Mumps && popd
+mkdir build
+pushd build
+../configure --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
+make
+make install
+tar -zcvf ipopt.tar.gz -C /home/travis/ipopt_install .
+export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('ipopt.tar.gz')"
+popd
+
+
+
---
+++ recipes/mingw-ipopt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+
+sudo apt-get update -qq
+sudo apt-get remove -qq -y mingw32
+sudo apt-get install -q -y mingw-w64
+sudo apt-get install -q -y mingw-w64 g++-mingw-w64 gcc-mingw-w64 gfortran-mingw-w64
+
+VERSION=3.12.3
+
+wget http://www.coin-or.org/download/source/Ipopt/Ipopt-$VERSION.tgz
+tar -xvf Ipopt-$VERSION.tgz
+pushd Ipopt-$VERSION
+pushd ASL && ./get.ASL && popd
+pushd Blas && ./get.Blas && popd 
+pushd Lapack && ./get.Lapack && popd 
+pushd Metis && ./get.Metis && popd 
+pushd Mumps && ./get.Mumps && popd
+mkdir build
+pushd build
+../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
+make
+make install
+tar -zcvf ipopt_mingw32.tar.gz -C /home/travis/ipopt_install .
+export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('ipopt_mingw32.tar.gz')"
+popd
+
+

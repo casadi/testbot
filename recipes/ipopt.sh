d500f76c89a672baac5dc20f441e4c51aa068b50
jgillis <joris.gillis42@gmail.com>
2015-05-26 07:27:57 +0200

    Update ipopt.sh

--- recipes/ipopt.sh
+++ recipes/ipopt.sh
@@ -17,11 +17,11 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
+../configure --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
 make
 make install
 popd && popd
-tar -zcvf ipopt.tar.gz -C /home/travis/ipopt_install .
+tar -zcvf ipopt.tar.gz -C /home/travis/ipopt-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('ipopt.tar.gz')"
 
 

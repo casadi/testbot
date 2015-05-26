8b7a08294bafd10e6b8e4eccdf2dff7672ca0629
jgillis <joris.gillis42@gmail.com>
2015-05-26 07:28:15 +0200

    Update mingw_ipopt.sh

--- recipes/mingw_ipopt.sh
+++ recipes/mingw_ipopt.sh
@@ -22,11 +22,11 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt_install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 popd && popd
-tar -zcvf ipopt_mingw32.tar.gz -C /home/travis/ipopt_install .
+tar -zcvf ipopt_mingw32.tar.gz -C /home/travis/ipopt-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('ipopt_mingw32.tar.gz')"
 
 

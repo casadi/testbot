e49e54d312d92c3bae42135f4a2ba111649e864a
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-08-13 16:26:56 +0200

    package

--- recipes/clang.sh
+++ recipes/clang.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+mypwd=`pwd`
+
 svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_342/final/ llvm
 cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_342/final/ clang
@@ -8,9 +10,9 @@ cd ../..
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release ../llvm
-make -j2
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$mypwd/install" ../llvm
+make install -j2
 
-pushd lib && tar -cvf $mypwd/clang.tar.gz . && popd
+pushd ../install && tar -cvf $mypwd/clang.tar.gz . && popd
 popd && popd
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('clang.tar.gz')"

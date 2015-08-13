680ebe855cdf6eab05112af3fcf6c95b0b3652e8
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-08-13 15:38:25 +0200

    yet older clang

--- recipes/clang.sh
+++ recipes/clang.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_362/final/ llvm
+svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_342/final/ llvm
 cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_362/final/ clang
+svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_342/final/ clang
 cd ../..
 mkdir build
 cd build

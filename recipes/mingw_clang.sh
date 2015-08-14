cbee0fe9589b289fcdc72957bf18097086cfff69
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-08-14 17:15:00 +0200

    work

--- recipes/mingw_clang.sh
+++ recipes/mingw_clang.sh
@@ -54,7 +54,7 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
 EOF
 
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CROSSCOMPILING=True -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
 make install -j2
 
 pushd ../install && tar -cvf $mypwd/clang_minwg32.tar.gz . && popd

28de9c52698d06811e8115184b53b4c022417a35
jgillis <joris.gillis42@gmail.com>
2015-09-13 13:59:53 +0200

    Update restricted.py

--- helpers/restricted.py
+++ helpers/restricted.py
@@ -21,6 +21,7 @@ def upload(filename):
 
   r = s.get('https://api.github.com/repos/jgillis/restricted/releases',timeout=timeout)
   assert r.ok, str(r)
+  print r.json()
   l = filter(lambda x: x["name"]=="Perpetual",r.json())
   release = l[0]
 

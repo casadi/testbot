fe381e61c17a0a2d3919795a75359f77e8095732
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-09-18 23:50:53 +0200

    work

--- recipes/mingw64_clang.sh
+++ recipes/mingw64_clang.sh
@@ -27,9 +27,6 @@ VERSION=342
 svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$VERSION/final/ llvm
 cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$VERSION/final/ clang
-cd ../projects
-svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/ libcxx
-cd libcxx && patch -p0 -i $mypwd/recipes/libcxx.patch && cd ..
 cd ../..
 mkdir build
 cd build
@@ -62,8 +59,10 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
 EOF
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DLLVM_ENABLE_THREADS=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
 make install -j2
+
+svn export http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/include $mypwd/install/include/casadi/jit/libc
 
 pushd ../install && tar -cvf $mypwd/clang_mingw64.tar.gz . && popd
 
--- recipes/mingw_clang.sh
+++ recipes/mingw_clang.sh
@@ -28,9 +28,9 @@ VERSION=342
 svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$VERSION/final/ llvm
 cd llvm/tools
 svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$VERSION/final/ clang
-cd ../projects
-svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/ libcxx
-cd libcxx && patch -p0 -i $mypwd/recipes/libcxx.patch && cd ..
+#cd ../projects
+#svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/ libcxx
+#cd libcxx && patch -p0 -i $mypwd/recipes/libcxx.patch && cd ..
 cd ../..
 mkdir build
 cd build
@@ -62,6 +62,8 @@ EOF
 
 cmake -DCMAKE_BUILD_TYPE=Release -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
 make install -j2
+
+svn export http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/include $mypwd/install/include/casadi/jit/libc
 
 pushd ../install && tar -cvf $mypwd/clang_mingw32.tar.gz . && popd
 

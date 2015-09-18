e58bc53f9c5cc963799ec6d7a38d0de481c3348b
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-09-19 00:28:17 +0200

    work

--- recipes/mingw_clang.sh
+++ recipes/mingw_clang.sh
@@ -63,7 +63,7 @@ EOF
 cmake -DCMAKE_BUILD_TYPE=Release -DCLANG_TABLEGEN=$mypwd/clang/bin/clang-tblgen -DLLVM_TABLEGEN=$mypwd/clang/bin/llvm-tblgen  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -DCMAKE_INSTALL_PREFIX="$mypwd/install"  -DCLANG_ENABLE_ARCMT=OFF -DCLANG_ENABLE_REWRITER=OFF -DCLANG_ENABLE_STATIC_ANALYZER=OFF  ../llvm
 make install -j2
 
-svn export http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/include $mypwd/install/include/casadi/jit/libc
+svn export http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/include $mypwd/install/include/c++/v1/
 
 pushd ../install && tar -cvf $mypwd/clang_mingw32.tar.gz . && popd
 

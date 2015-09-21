68dfe61effb560a084073873a83032fc9fd3b5fd
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-09-21 17:25:49 +0200

    test

--- recipes/gcc47.sh
+++ recipes/gcc47.sh
@@ -4,14 +4,14 @@ set -e
 mypwd=`pwd`
 
 sudo apt-get install libc6-dev libc6-dev-i386
-sudo apt-get install lbmpfr-dev ibmpc-dev flex bison
+sudo apt-get install libmpfr-dev libmpc-dev flex bison
 
 svn co svn://gcc.gnu.org/svn/gcc/tags/gcc_4_7_4_release/ gcc-4.7
 cd gcc-4.7
 
 mkdir build
 cd build
-../configure --prefix="$mypwd/install" --disable-nls --enable-languages=c,c++
+../configure --prefix="$mypwd/install" --disable-nls --enable-languages=c,c++,fortran
 make -j2
 make install
 

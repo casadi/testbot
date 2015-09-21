173cbe6bfc83399e7ee0cd02a5bcf0f9954a186b
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-09-21 18:31:09 +0200

    test

--- recipes/gcc47.sh
+++ recipes/gcc47.sh
@@ -6,7 +6,7 @@ mypwd=`pwd`
 sudo apt-get install libc6-dev libc6-dev-i386
 sudo apt-get install libmpfr-dev libmpc-dev flex bison
 
-svn co svn://gcc.gnu.org/svn/gcc/tags/gcc_4_7_4_release/ gcc-4.7
+svn co -q svn://gcc.gnu.org/svn/gcc/tags/gcc_4_7_4_release/ gcc-4.7
 cd gcc-4.7
 
 mkdir build

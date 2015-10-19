12cdbdfbdb68488c3b267fc02bab8a3a8a4429b9
jgillis <joris.gillis42@gmail.com>
2015-10-19 15:45:08 +0200

    Update mingw_ipopt.sh

--- recipes/mingw_ipopt.sh
+++ recipes/mingw_ipopt.sh
@@ -22,7 +22,7 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --host i686-w64-mingw32  --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+../configure --host i686-w64-mingw32 --build mingw32 --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 popd && popd

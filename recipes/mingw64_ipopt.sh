d473d1bda12047fdd67dde9d89b2a59b71e4a8c7
jgillis <joris.gillis42@gmail.com>
2015-10-19 15:39:01 +0200

    Update mingw64_ipopt.sh

--- recipes/mingw64_ipopt.sh
+++ recipes/mingw64_ipopt.sh
@@ -22,7 +22,8 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --host x86_64-w64-mingw32  --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+# build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
+../configure --host x86_64-w64-mingw32 --build mingw32 --prefix=/home/travis/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 popd && popd

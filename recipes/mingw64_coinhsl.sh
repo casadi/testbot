a7d06fc756787f06aecb95a7df740b19154ba87c
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-10-21 14:58:25 +0200

    work

--- recipes/mingw64_coinhsl.sh
+++ recipes/mingw64_coinhsl.sh
@@ -8,15 +8,17 @@ sudo apt-get remove -qq -y mingw32
 sudo apt-get install -q -y mingw-w64
 sudo apt-get install -q -y mingw-w64 g++-mingw-w64 gcc-mingw-w64 gfortran-mingw-w64
 
-export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; download('ipopt_mingw64.zip')"
+testbot/recipes/fetch.sh matlab$MATLABRELEASE.tar.gz
+
+export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; download('ipopt_mingw64.tar.gz')"
 mkdir ipopt && tar -xf ipopt_mingw64.tar.gz -C ipopt
-pushd /home/travis/ && ln -s  /home/travis/build/ipopt ipopt-install  && popd
+pushd /home/travis/ && ln -s $mypwd/ipopt ipopt-install  && popd
 
 pushd restricted
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
 tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz
 
-./configure --host x86_64-w64-mingw32 --prefix=/home/travis/build/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="-fopenmp" FCFLAGS="-O2 -fopenmp" CFLAGS="-O2 -fopenmp"
+./configure --host x86_64-w64-mingw32 --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="-fopenmp" FCFLAGS="-O2 -fopenmp" CFLAGS="-O2 -fopenmp"
 sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
 make
 make install

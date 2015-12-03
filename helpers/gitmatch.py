e253503b90ff9bfc369f295f79bb69d2ab16c3ad
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-12-03 10:01:44 +0100

    non-numpy inf

--- helpers/gitmatch.py
+++ helpers/gitmatch.py
@@ -2,7 +2,7 @@ import subprocess
 
 import sys
 import yaml
-import numpy as np
+inf = float('Inf')
 
 config = yaml.load(file(sys.argv[1],'r'))
 
@@ -10,10 +10,10 @@ def score(s):
   if s=="default": return 1e9
   p = subprocess.Popen(["git","merge-base","--is-ancestor",s, "HEAD"],cwd=sys.argv[2])
   p.wait()
-  if p.returncode!=0: return np.inf
+  if p.returncode!=0: return inf
   p = subprocess.Popen(["git","rev-list","--ancestry-path","%s..HEAD" % s, "--count"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=sys.argv[2])
   out,err = p.communicate()
-  return int(out) if p.returncode==0 else np.inf
+  return int(out) if p.returncode==0 else inf
 
 print sorted([(k,v,score(k)) for k,v in config.items()],key=lambda x:x[2])[0][1]
 

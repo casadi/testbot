2a66adf882909627279ff9f14ab9a2c9faa08a17
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-12-03 10:16:07 +0100

    work

--- recipes/swig_matlab_fixed.sh
+++ recipes/swig_matlab_fixed.sh
@@ -2,8 +2,8 @@
 set -e
 sudo apt-get install -y libpcre3-dev automake yodl
 mypwd=`pwd`
-pushd restricted && git clone https://github.com/jaeandersson/swig.git && git checkout 82714bf35c33fe2
-pushd swig && git checkout matlab && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
+pushd restricted && git clone https://github.com/jaeandersson/swig.git
+pushd swig && git checkout 82714bf35c33fe2 && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
 popd && popd
 tar -zcvf swig_matlab.tar.gz -C swig-matlab-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig_matlab_fixed.tar.gz')"

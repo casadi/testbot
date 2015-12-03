bac5f3a3dc3583514369c80d11c1c323dcea8a5f
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-12-03 08:27:50 +0100

    build in some versioning for binaries

---
+++ helpers/gitmatch.py
@@ -0,0 +1,23 @@
+import subprocess
+
+import sys
+import yaml
+import numpy as np
+
+config = yaml.load(file(sys.argv[1],'r'))
+
+def score(s):
+  if s=="default": return 1e9
+  p = subprocess.Popen(["git","merge-base","--is-ancestor",s, "HEAD"],cwd=sys.argv[2])
+  p.wait()
+  if p.returncode!=0: return np.inf
+  p = subprocess.Popen(["git","rev-list","--ancestry-path","%s..HEAD" % s, "--count"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=sys.argv[2])
+  out,err = p.communicate()
+  return int(out) if p.returncode==0 else np.inf
+
+print sorted([(k,v,score(k)) for k,v in config.items()],key=lambda x:x[2])[0][1]
+
+
+
+
+

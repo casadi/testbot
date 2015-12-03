fa647c8223dce75525d20e7c0dd28d9ca872d382
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-12-03 10:21:32 +0100

    work

--- recipes/swig_matlab_fixed.sh
+++ recipes/swig_matlab_fixed.sh
@@ -5,5 +5,5 @@ mypwd=`pwd`
 pushd restricted && git clone https://github.com/jaeandersson/swig.git
 pushd swig && git checkout 82714bf35c33fe2 && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
 popd && popd
-tar -zcvf swig_matlab.tar.gz -C swig-matlab-install .
+tar -zcvf swig_matlab_fixed.tar.gz -C swig-matlab-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig_matlab_fixed.tar.gz')"

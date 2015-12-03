a78c8a76ed64a26d2d80025646f777556cec264c
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2015-12-03 13:13:57 +0100

    yodl fix?

--- recipes/swig_matlab_fixed_osx.sh
+++ recipes/swig_matlab_fixed_osx.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 brew install pcre automake
+
+function yodl2man {
+                    touch ${@: -1}
+                }
 mypwd=`pwd`
 pushd restricted && git clone https://github.com/jaeandersson/swig.git
 pushd swig && git checkout 82714bf35c33fe2 && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install

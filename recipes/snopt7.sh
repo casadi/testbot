9d63a1f977a72e35fe5b7b20f7c18a6deed32a2c
Joel Andersson <j.a.e.andersson@gmail.com>
2016-01-08 08:57:59 -0600

    Added compilation of C and C++ interfaces to SNOPT

--- recipes/snopt7.sh
+++ recipes/snopt7.sh
@@ -9,7 +9,7 @@ mypwd=`pwd`
 #sudo mv /usr/lib/libf2c.so /usr/lib/libf2c.so_backup
 #sudo ln -s /usr/lib/libf2c.a /usr/lib/libf2c.so
 mkdir "$mypwd/snopt7-install"
-pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --prefix "$mypwd/snopt7-install" && make && make install && popd
+pushd restricted && tar -xvf snopt7.tar.gz && cd snopt7 && ./configure --with-c-cpp=yes --prefix "$mypwd/snopt7-install" && make && make install && popd
 ls -al "$mypwd/snopt7-install/lib"
 git clone https://github.com/jgillis/snopt-interface.git
 pushd snopt-interface && ./autogen.sh && ./configure --with-snopt="$mypwd/snopt7-install/lib" --prefix="$mypwd/snopt7-install" && make lib && make install && popd

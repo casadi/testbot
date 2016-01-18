36e60f936ec79941eb9e38cc040944923ab6fb83
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-01-18 22:27:32 +0100

    work

--- recipes/clang_osx.sh
+++ recipes/clang_osx.sh
@@ -15,7 +15,7 @@ cd ../..
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$mypwd/install" ../llvm
+cmake -DLLVM_ENABLE_TERMINFO=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$mypwd/install" ../llvm
 make clang-tblgen install -j2
 cp bin/clang-tblgen "$mypwd/install/bin"
 

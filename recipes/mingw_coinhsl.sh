e8540e3a56d8a85eaf1c3cbd193d1136efe5a5bc
jgillis <joris.gillis42@gmail.com>
2016-01-31 15:54:22 +0100

    Update mingw_coinhsl.sh

--- recipes/mingw_coinhsl.sh
+++ recipes/mingw_coinhsl.sh
@@ -31,7 +31,7 @@ pushd restricted
 wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
 tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz
 
-./configure --host $compilerprefix --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="" FCFLAGS="-O2" CFLAGS="-O2" || cat config.log
+./configure --disable-static --enable-shared --host $compilerprefix --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="" FCFLAGS="-O2" CFLAGS="-O2" || cat config.log
 sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
 make
 make install

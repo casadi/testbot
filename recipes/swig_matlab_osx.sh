90fb40c6a938c507b8c162f596f28df13ca67e5d
jgillis <joris.gillis42@gmail.com>
2016-02-22 22:15:12 +0100

    Update swig_matlab_osx.sh

--- recipes/swig_matlab_osx.sh
+++ recipes/swig_matlab_osx.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:`pwd`
 
 mypwd=`pwd`
 pushd restricted && git clone https://github.com/jaeandersson/swig.git
-pushd swig && git checkout master && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
+pushd swig && git checkout matlab && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
 popd && popd
 tar -zcvf swig_matlab_osx.tar.gz -C swig-matlab-install .
 export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig_matlab_osx.tar.gz')"

cc91b3c6bf44a50adbd6524b524627dcf6db2d19
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-02-22 21:58:40 +0100

    work

---
+++ recipes/swig_matlab_osx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+brew install pcre automake
+
+echo "touch \${@: -2}" > yodl2man && chmod +x yodl2man
+export PATH=$PATH:`pwd`
+
+mypwd=`pwd`
+pushd restricted && git clone https://github.com/jaeandersson/swig.git
+pushd swig && git checkout master && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
+popd && popd
+tar -zcvf swig_matlab_osx.tar.gz -C swig-matlab-install .
+export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig_matlab_osx.tar.gz')"

304e34cc43819767325a09d1f7759406a1b0f186
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-05-25 17:09:15 +0200

    work

--- recipes/bonmin_osx.sh
+++ recipes/bonmin_osx.sh
@@ -16,7 +16,7 @@ pushd Mumps && ./get.Mumps && popd
 popd
 mkdir build
 pushd build
-../configure --prefix=/Users/travis/bonmin-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --with-asl=BUILD
+../configure --prefix=/Users/travis/bonmin-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 popd && popd

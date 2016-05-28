d354acb76e48de53b0330407f8486e0d5098340f
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-05-28 08:02:36 +0200

    work

--- recipes/mingw64_bonmin.sh
+++ recipes/mingw64_bonmin.sh
@@ -22,11 +22,11 @@ popd
 mkdir build
 pushd build
 # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
-../configure --host x86_64-w64-mingw32 --enable-dependency-linking --build mingw32 --prefix=/home/travis/bonmin-install --disable-static --enable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
+../configure --host x86_64-w64-mingw32 --enable-dependency-linking --build mingw32 --prefix=/home/travis/bonmin-install --disable-shared --enable-static ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
 make
 make install
 popd && popd
 tar -zcvf bonmin_mingw64_shared.tar.gz -C /home/travis/bonmin-install .
-export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('bonmin_mingw64_shared.tar.gz')"
+export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('bonmin_mingw64.tar.gz')"
 
 

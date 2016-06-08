31a1707862fc7a7aeaa278f5bf3cfbb7dee683f4
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-08 11:36:21 +0200

    work

--- recipes/setup.sh
+++ recipes/setup.sh
@@ -42,7 +42,7 @@ function slurp() {
   if [ -f $HOME/build/testbot/recipes/$1_${SLURP_CROSS}${BITNESS}_${SLURP_OS}.sh ];
   then
     SETUP=1 source $HOME/build/testbot/recipes/$1_${SLURP_CROSS}${BITNESS}_${SLURP_OS}.sh
-  if [ -f $HOME/build/testbot/recipes/$1_${SLURP_CROSS}_${SLURP_OS}.sh ];
+  elif [ -f $HOME/build/testbot/recipes/$1_${SLURP_CROSS}_${SLURP_OS}.sh ];
   then
     SETUP=1 source $HOME/build/testbot/recipes/$1_${SLURP_CROSS}_${SLURP_OS}.sh
   else

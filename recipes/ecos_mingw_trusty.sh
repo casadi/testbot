e18869498990dc5e991679bb2bee616fab25ccb2
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-08 15:49:31 +0200

    work

--- recipes/ecos_mingw_trusty.sh
+++ recipes/ecos_mingw_trusty.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+export SUFFIX=mingw${BITNESS}_trusty
+
 if [ -z "$SETUP" ]; then
   mingw_setup
   export FLAGS="ISWINDOWS=1 CC=$compilerprefix-gcc AR=$compilerprefix-ar RANLIB=$compilerprefix-ranlib"
-  export SUFFIX=mingw${BITNESS}_trusty
   export SUFFIXFILE=_$SUFFIX
 fi
 
--- recipes/ipopt_mingw_trusty.sh
+++ recipes/ipopt_mingw_trusty.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-
+export SUFFIX=mingw${BITNESS}_trusty
 
 if [ -z "$SETUP" ]; then
   mingw_setup
-  export SUFFIX=mingw${BITNESS}_trusty
   export SUFFIXFILE=_$SUFFIX
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
   export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32"

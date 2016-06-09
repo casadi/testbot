aa0544079c36f14640927a9db4212067175cd034
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-09 15:23:59 +0200

    work

---
+++ recipes/swig_matlab_trusty.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ -z "$SETUP" ]; then
+  sudo apt-get install -y libpcre3-dev automake yodl
+  mypwd=`pwd`
+  pushd restricted && git clone https://github.com/jaeandersson/swig.git
+  pushd swig && git checkout matlab && ./autogen.sh && ./configure --prefix=$mypwd/swig-matlab-install && make && make install
+  popd && popd
+  tar -zcvf swig_matlab_trusty.tar.gz -C swig-matlab-install .
+  export PYTHONPATH="$PYTHONPATH:$mypwd/helpers" && python -c "from restricted import *; upload('swig_matlab_trusty.tar.gz')"
+
+else
+  fetch_tar swig_matlab trusty
+  pushd $HOME/build/casadi/testbot && ln -s  $HOME/build/swig_matlab  swig-matlab-install  && popd
+  export PATH=$HOME/build/swig_matlab/bin:$PATH
+fi

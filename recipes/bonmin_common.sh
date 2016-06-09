8dce5a7c3afe1b798a55577a2900a7020af177af
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-09 09:12:25 +0200

    work

--- recipes/bonmin_common.sh
+++ recipes/bonmin_common.sh
@@ -20,7 +20,8 @@ if [ -z "$SETUP" ]; then
   pushd build
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
   ../configure $FLAGS --prefix=$HOME/bonmin-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
-  make
+  (while true ; do sleep 60 ; echo "ping" ; done ) &
+  make > /dev/null
   make install
   popd && popd
   tar -zcvf bonmin$SUFFIXFILE.tar.gz -C $HOME/bonmin-install .

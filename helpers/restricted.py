7fc6099fe1630b48266de3dd60bd3ad129791179
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-10 09:35:40 +0200

    work

--- helpers/restricted.py
+++ helpers/restricted.py
@@ -32,13 +32,14 @@ def upload(filename,remote_filename=None):
   time.sleep(1)
 
   for a in assets.json():
-    if a["name"]==filename:
+    if a["name"]==remote_filename:
      print "Overwriting"
      r = s.delete(a["url"])
      assert r.ok, str(r)
      time.sleep(1)
         
 
+  print "Uploading", filename, "to", remote_filename
   rs = s.post(release["upload_url"].replace("{?name,label}",""),params={"name": remote_filename,"label": ""},data=file(filename,"rb"),verify=False,headers={"Content-Type":"application/gzip"},timeout=timeout)
   assert rs.ok, str(rs.json())
   return rs

c80eda18b8d547f2b18dd31a695ee7813b1bec7d
jgillis <joris.gillis42@gmail.com>
2016-06-23 10:41:23 +0200

    Update mingw_octave_trusty.sh

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -2,12 +2,13 @@
 set -e
 
 if [ -z "$SETUP" ]; then
+  mypwd=`pwd`
   hg clone http://hg.octave.org/mxe-octave/
   cd mxe-octave
   autoconf
   ./configure
-  make -j2
-  
-
+  make mingw-w64
+  tar -zcvf mingw_octave_trusty.tar.gz -C $mypwd .
 else
+  export PATH=$HOME/build/mingw_octave:$PATH
 fi

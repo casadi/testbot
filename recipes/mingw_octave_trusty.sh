13dd048a7f3376b52b44e3c44d2c325e9c7810ea
jgillis <joris.gillis42@gmail.com>
2016-06-23 14:15:29 +0200

    Update mingw_octave_trusty.sh

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -3,12 +3,12 @@ set -e
 
 if [ -z "$SETUP" ]; then
   mypwd=`pwd`
-  sudo apt-get install -y automake
   hg clone http://hg.octave.org/mxe-octave/
   cd mxe-octave
   autoconf
   ./configure
   (while true ; do sleep 60 ; echo "ping" ; done ) &
+  automake --add-missing
   make -j2 mingw-w64
   tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd .
   slurp_put mingw_octave$SUFFIXFILE

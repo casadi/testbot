92ab0cf85bd2836f4a092a9e9614cac909fcfc13
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-06-24 10:57:55 +0200

    work

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -9,9 +9,9 @@ if [ -z "$SETUP" ]; then
   ./configure
   (while true ; do sleep 60 ; echo "ping" ; done ) &
   make mingw-w64
-  tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd .
+  tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd usr >dev/null
   slurp_put mingw_octave$SUFFIXFILE
 else
   fetch_tar mingw_octave $SUFFIX
-  export PATH=$HOME/build/mingw_octave:$PATH
+  export PATH=$HOME/build/mingw_octave/usr/bin:$PATH
 fi

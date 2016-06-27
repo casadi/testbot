47b0d36c9c5f9c43f4f94843309128c21005df6e
jgillis <joris.gillis42@gmail.com>
2016-06-27 08:55:00 +0200

    Update mingw_octave_trusty.sh

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -10,7 +10,8 @@ if [ -z "$SETUP" ]; then
   (while true ; do sleep 60 ; echo "ping" ; done ) &
   make mingw-w64
   ls -al
-  tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd usr >/dev/null
+  echo "tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd usr /dev/null"
+  tar -zcvf mingw_octave$SUFFIXFILE.tar.gz usr >/dev/null
   slurp_put mingw_octave$SUFFIXFILE
 else
   fetch_tar mingw_octave $SUFFIX

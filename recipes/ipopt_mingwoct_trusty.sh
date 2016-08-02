04c5b0a93d1af57002b8cc04e057c0f3aff655f7
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-08-02 21:42:16 +0200

    work

---
+++ recipes/ipopt_mingwoct_trusty.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export SUFFIX=mingwoct${BITNESS}_trusty
+
+if [ -z "$SETUP" ]; then
+  export SLURP_OS=trusty
+  export SLURP_CROSS=mingw
+  slurp mingw_octave
+  export SUFFIXFILE=_$SUFFIX
+  # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
+  export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32"
+fi
+
+source $RECIPES_FOLDER/ipopt_common.sh
+

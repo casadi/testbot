f77f84bc9be799934653cdda0f642acead15e573
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-08-03 07:41:21 +0200

    work

--- recipes/fetch.sh
+++ recipes/fetch.sh
@@ -2,5 +2,5 @@
 set -e
 
 mypwd=`pwd`
-export PYTHONPATH="$PYTHONPATH:$mypwd/testbot/helpers:$mypwd/testbot" && python -c "from restricted import *; download('$1')"
+export PYTHONPATH="$PYTHONPATH:$TESTBOT_DIR/helpers:$TESTBOT_DIR" && python -c "from restricted import *; download('$1')"
 

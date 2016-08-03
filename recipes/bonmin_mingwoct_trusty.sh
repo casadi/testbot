6770f82f2bde70ccefc68099f381849ca5051cdd
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-08-03 08:20:56 +0200

    work

---
+++ recipes/bonmin_mingwoct_trusty.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SUFFIX=mingwoct${BITNESS}_trusty
+
+if [ -z "$SETUP" ]; then
+  export SLURP_OS=trusty
+  slurp mingw_octave
+  export SUFFIXFILE=_$SUFFIX
+  # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
+  export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32 coin_skip_warn_cxxflags=yes"
+fi
+
+source $RECIPES_FOLDER/bonmin_common.sh
---
+++ recipes/lapack_mingwoct_trusty.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$SETUP" ]; then
+
+
+  mypwd=`pwd`
+
+  export SLURP_OS=trusty
+  slurp mingw_octave
+
+  VERSION=3.4.2
+
+  wget http://www.coin-or.org/BuildTools/Lapack/lapack-$VERSION.tgz
+  tar -xvf lapack-$VERSION.tgz
+  pushd lapack-$VERSION
+  mkdir build
+  pushd build
+
+cat <<EOF >toolchain.cmake
+# this one is important
+SET(CMAKE_SYSTEM_NAME Windows)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+# specify the cross compiler
+
+set(COMPILER_PREFIX "$compilerprefix")
+
+SET(CMAKE_C_COMPILER $compilerprefix-gcc)
+SET(CMAKE_CXX_COMPILER $compilerprefix-g++)
+SET(CMAKE_Fortran_COMPILER $compilerprefix-gfortran)
+set(CMAKE_RC_COMPILER $compilerprefix-windres)
+set(CMAKE_RANLIB $compilerprefix-ranlib)
+set(CMAKE_AR $compilerprefix-ar)
+
+# where is the target environment
+SET(CMAKE_FIND_ROOT_PATH /usr/$compilerprefix)
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE NEVER)
+EOF
+
+  cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake ..
+  make lapack -j2
+  pushd lib && tar -cvf $mypwd/lapack_mingw${BITNESS}_trusty.tar.gz . && popd
+  popd && popd
+  slurp_put lapack_mingwoct${BITNESS}_trusty
+
+else
+  fetch_tar lapack mingwoct${BITNESS}_trusty
+  export LIB=$HOME/build/lapack
+fi
+

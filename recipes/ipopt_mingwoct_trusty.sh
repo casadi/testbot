ea2de6e12d862aa595b8c917d09d4c9bb74a8c5f
jgillis <joris.gillis42@gmail.com>
2016-08-04 08:25:18 +0200

    Update ipopt_mingwoct_trusty.sh

--- recipes/ipopt_mingwoct_trusty.sh
+++ recipes/ipopt_mingwoct_trusty.sh
@@ -9,6 +9,7 @@ if [ -z "$SETUP" ]; then
   echo "ipopt_mingw_end:$BAKEVERSION:"
   export SUFFIXFILE=_$SUFFIX
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
+  i686-w64-mingw32-gcc --version
   export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32 coin_skip_warn_cxxflags=yes"
 fi
 

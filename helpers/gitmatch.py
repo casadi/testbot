cd9ba4b35a0adaa8c43711220a979aa52a6ace40
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-08-04 16:35:15 +0200

    work

--- helpers/gitmatch.py
+++ helpers/gitmatch.py
@@ -8,11 +8,16 @@ config = yaml.load(file(sys.argv[1],'r'))
 
 def score(s):
   if s=="default": return 1e9
+  print >>sys.stderr, "check if in ancestry", s
   p = subprocess.Popen(["git","merge-base","--is-ancestor",s, "HEAD"],cwd=sys.argv[2])
   p.wait()
-  if p.returncode!=0: return inf
+  if p.returncode!=0:
+    print >>sys.stderr,"not found"
+    return inf
+  print "check score", s
   p = subprocess.Popen(["git","rev-list","--ancestry-path","%s..HEAD" % s, "--count"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,cwd=sys.argv[2])
   out,err = p.communicate()
+  print >>sys.stderr, out
   return int(out) if p.returncode==0 else inf
 
 print sorted([(k,v,score(k)) for k,v in config.items()],key=lambda x:x[2])[0][1]

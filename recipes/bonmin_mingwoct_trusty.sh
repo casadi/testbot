e7e51f39fc31a7794a06f27d52d7bcce8b56bdfd
jgillis <joris.gillis42@gmail.com>
2016-08-04 09:05:15 +0200

    Update bonmin_mingwoct_trusty.sh

--- recipes/bonmin_mingwoct_trusty.sh
+++ recipes/bonmin_mingwoct_trusty.sh
@@ -7,7 +7,7 @@ if [ -z "$SETUP" ]; then
   slurp mingw_octave
   export SUFFIXFILE=_$SUFFIX
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
-  export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32 coin_skip_warn_cxxflags=yes"
+  export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32 coin_skip_warn_cxxflags=yes --disable-pkg-config"
 fi
 
 source $RECIPES_FOLDER/bonmin_common.sh

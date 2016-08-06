73d382c8c3323d7bcd20a2b08e18a1994fe76889
jgillis <joris.gillis42@gmail.com>
2016-08-06 22:07:07 +0200

    Update swig_trusty.sh

--- recipes/swig_trusty.sh
+++ recipes/swig_trusty.sh
@@ -17,6 +17,7 @@ else
   pushd $HOME/build/casadi/testbot && ln -s  $HOME/build/swig_matlab  swig-matlab-install  && popd
   export PATH=$HOME/build/swig_matlab/bin:$HOME/build/swig_matlab/share:$PATH
   function swig_patch_pyobject() {
-    sed -i -e 's/\"SwigPyObject\"/\"SwigPyCasadiObject\"/g' $SWIG_HOME/share/swig/2.0.12/python/pyrun.swg
+    #sed -i -e 's/\"SwigPyObject\"/\"SwigPyCasadiObject\"/g' $SWIG_HOME/share/swig/2.0.12/python/pyrun.swg
+    sed -i -e 's/\"SwigPyObject\"/\"SwigPyCasadiObject\"/g' $SWIG_HOME/share/swig/*/python/pyrun.swg
   }
 fi

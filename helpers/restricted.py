c715ba25cdc6efaffe15fc1d23ea27c8b55e0c3f
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-09-02 08:22:50 +0200

    multi-page asset dowloader

--- helpers/restricted.py
+++ helpers/restricted.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 import os
-
+import re
 
 timeout = 100
 
@@ -18,6 +18,22 @@ s.headers.update({'Accept': 'application/vnd.github.manifold-preview'})
 
 myparams = {"per_page": 100}
 
+def get_assets(url,**kwargs):
+  while True:
+    assets = s.get(url,timeout=timeout,params=myparams)
+    assert(assets.ok), str(assets)
+    time.sleep(1)
+    for a in assets.json():
+      yield a
+    nl = [i for i in assets.headers["Link"].split(",") if "next" in i]
+    if len(nl)==0:
+      break
+    if len(nl)>1:
+      raise Exception("Unexpected header" + assets.headers["Link"])
+    nl = nl[0]
+    url = nl.split(";")[0][1:-1]
+  
+ 
 def upload(filename,remote_filename=None):
   if remote_filename is None:
     remote_filename = filename
@@ -27,11 +43,7 @@ def upload(filename,remote_filename=None):
   l = filter(lambda x: x["name"]=="Perpetual",r.json())
   release = l[0]
 
-  assets = s.get(release["assets_url"],timeout=timeout,params=myparams)
-  assert(assets.ok), str(assets)
-  time.sleep(1)
-
-  for a in assets.json():
+  for a in get_assets(release["assets_url"],timeout=timeout,params=myparams):
     if a["name"]==remote_filename:
      print "Overwriting"
      r = s.delete(a["url"])
@@ -50,11 +62,7 @@ def download(filename):
   l = filter(lambda x: x["name"]=="Perpetual",r.json())
   release = l[0]
 
-  assets = s.get(release["assets_url"],timeout=timeout,params=myparams)
-  assert(assets.ok), str(assets)
-  time.sleep(1)
-
-  for a in assets.json():
+  for a in get_assets(release["assets_url"],timeout=timeout,params=myparams):
     if a["name"]==filename:
       rs = s.get(a["url"], stream=True,verify=False,headers={"Accept":"application/octet-stream"},timeout=timeout)
       rs = requests.get(rs.history[0].headers["location"],stream = True)

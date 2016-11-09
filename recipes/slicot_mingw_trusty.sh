2d82243496df11b373e4019e876216bc076df827
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-11-09 12:39:22 +0100

    Adding slicot build

---
+++ recipes/slicot_mingw_trusty.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$SETUP" ]; then
+
+
+  export SLURP_CROSS=mingw
+  export SLURP_OS=trusty
+
+  slurp lapack_mingw
+
+  mypwd=`pwd`
+
+  mingw_setup
+
+  git clone https://anonscm.debian.org/git/debian-science/packages/slicot.git
+
+  cd slicot
+
+  cat <<EOF >makefile
+  F77=$compilerprefix-gfortran
+  LDFLAGS += -Wl,--as-needed
+
+  SLICOT_SRC=$(sort $(shell echo src/*.f))
+  SLICOT_OBJ=$(SLICOT_SRC:.f=.o)
+
+  src/%.o : src/%.f
+          $(F77) $(FFLAGS) -fPIC -c $< -o $@
+
+  libslicot.dll: $(SLICOT_OBJ)
+          $(F77) $(LDFLAGS) -shared -Wl,-soname=libslicot.dll -o $@ $^ -L$LIB -llapack -lblas -llapack
+EOF
+
+  make
+  pushd lib && tar -cvf $mypwd/slicot${BITNESS}_trusty.tar.gz . && popd
+  popd && popd
+  slurp_put slicot${BITNESS}_trusty
+
+else
+  fetch_tar slicot${BITNESS}_trusty
+  export SLICOT_LIBRARY_DIR=$HOME/build/slicot
+fi

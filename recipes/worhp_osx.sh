5715d518864db98c69f1810ee459d4915257f2f1
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-25 11:23:02 +0100

    work

--- recipes/worhp_osx.sh
+++ recipes/worhp_osx.sh
@@ -4,7 +4,8 @@ set -e
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  try_fetch_7z libworhp_macosx10.9_gcc49_1.8-3cdb565 worhp
+  #try_fetch_7z libworhp_macosx10.9_gcc49_1.8-3cdb565 worhp
+  try_fetch_7z libworhp_macosx10.9_gcc48_1.8-3cdb565 worhp
   export WORHP=$HOME/build/worhp
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
 fi

47e2f333510ce1b86188108d4fcacbf165f3ff22
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-25 14:07:50 +0100

    work

---
+++ fetch_common.bat
@@ -0,0 +1,8 @@
+python -c "from restricted import *; download('hsl_mingw%BITNESS%_trusty.zip')"
+7z x -y "hsl_mingw%BITNESS%_trusty.zip" > nul
+python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip')"
+7z x -y "slicot_mingw%BITNESS%_trusty.zip" > nul
+python -c "from restricted import *; download('libworhp_vs2013_1.9-1_16_10.7z')"
+7z x -y "libworhp_vs2013_1.9-1_16_10.7z" > nul
+dir
+set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\vs2013-Release\bin%BITNESS%

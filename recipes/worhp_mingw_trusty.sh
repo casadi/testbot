1528c745cab156374cc810c5c7d040caf58cbb54
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-25 10:46:44 +0100

    work

--- recipes/worhp_mingw_trusty.sh
+++ recipes/worhp_mingw_trusty.sh
@@ -5,6 +5,8 @@ if [ -z "$SETUP" ]; then
   echo "noop"
 else
   try_fetch_7z libworhp_vs2013_1.9-1_16_10 worhp
-  export WORHP=$HOME/build/worhp
+  echo "bitness: ${BITNESS}"
+  export WORHP=$HOME/build/worhp/vs2013-Release/
+  mv $WORHP/bin${BITNESS} $WORHP/bin
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
 fi

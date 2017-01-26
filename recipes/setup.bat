e2f667c3c5611e180a34fc1794d1bdedd1d9a36c
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-26 13:21:21 +0100

    work

--- recipes/setup.bat
+++ recipes/setup.bat
@@ -1,6 +1,6 @@
 openssl aes-256-cbc -k "%keyA%" -in id_rsa_travis.enc -out id_rsa_travis -d
 openssl aes-256-cbc -k "%keyA%" -in testbotcredentials.py.enc -out testbotcredentials.py -d
 
-set PYTHONPATH=%PYTHONPATH%:%cd%
+set PYTHONPATH=%PYTHONPATH%;%cd%
 
 git clone "ext::ssh -i $(%cd%/id_rsa_travis  git@github.com %S jgillis/restricted.git" 

229c34621e54425e822ce8c8ee19da2337f6e238
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-26 14:29:39 +0100

    bump worhp

--- recipes/worhp_trusty.sh
+++ recipes/worhp_trusty.sh
@@ -4,7 +4,7 @@ set -e
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  try_fetch_7z libworhp_debian7_amd64_gcc47_1.7-12970f3 worhp
+  try_fetch_7z libworhp_ubuntu14.04_amd64_1.9-1_16_10 worhp
   export WORHP=$HOME/build/worhp
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
 fi

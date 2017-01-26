bb33ec871433cf60c64f61e395db0bad25c2b1df
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-27 00:05:20 +0100

    work

--- recipes/worhp_mingw_trusty.sh
+++ recipes/worhp_mingw_trusty.sh
@@ -6,10 +6,10 @@ if [ -z "$SETUP" ]; then
 else
   if [ "${BITNESS}" = "32" ]; then
     try_fetch_7z libworhp_mingw-w64_i686_1.7-dd7abd3 worhp
-  do
+  fi
   if [ "${BITNESS}" = "64" ]; then
     try_fetch_7z libworhp_mingw-w64_x86_64_1.7-dd7abd3 worhp
-  do
+  fi
   echo "bitness: ${BITNESS}"
   ls $HOME/build/worhp
   export WORHP=$HOME/build/worhp/

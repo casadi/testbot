fdcec76ceb57c95343172376044f03717de7f24a
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-26 23:51:31 +0100

    work

--- recipes/worhp_mingw_trusty.sh
+++ recipes/worhp_mingw_trusty.sh
@@ -4,10 +4,15 @@ set -e
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  try_fetch_7z libworhp_vs2013_1.9-1_16_10 worhp
+  if [ "${BITNESS}" = "32" ]; then
+    try_fetch_7z libworhp_mingw-w64_i686_1.7-dd7abd3 worhp
+  do
+  if [ "${BITNESS}" = "64" ]; then
+    try_fetch_7z libworhp_mingw-w64_x86_64_1.7-dd7abd3 worhp
+  do
   echo "bitness: ${BITNESS}"
   ls $HOME/build/worhp
-  export WORHP=$HOME/build/worhp/vs2013-Release/
-  mv $WORHP/bin${BITNESS} $WORHP/lib
+  export WORHP=$HOME/build/worhp/
+  cp $WORHP/bin/* $WORHP/lib
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
 fi

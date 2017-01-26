97913198b74374c666a7a3c16d7e8971fea61be7
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-26 13:22:10 +0100

    work

--- recipes/setup.bat
+++ recipes/setup.bat
@@ -1,3 +1,5 @@
+echo "hello"
+
 openssl aes-256-cbc -k "%keyA%" -in id_rsa_travis.enc -out id_rsa_travis -d
 openssl aes-256-cbc -k "%keyA%" -in testbotcredentials.py.enc -out testbotcredentials.py -d
 

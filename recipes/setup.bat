de53079a73d44db36cf28fcf1e9b995f3d55858c
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-26 14:24:16 +0100

    work

--- recipes/setup.bat
+++ recipes/setup.bat
@@ -1,7 +1,7 @@
 openssl aes-256-cbc -k "%keyA%" -in id_rsa_travis.enc -out id_rsa_travis -d
 openssl aes-256-cbc -k "%keyA%" -in testbotcredentials.py.enc -out testbotcredentials.py -d
-openssl aes-256-cbc -k "%keyA%" -in testbot/env.bat.enc -out env.bat -d
-openssl aes-256-cbc -k "%keyA%" -in testbot/id_rsa_travis.pkk.enc -out id_rsa_travis.pkk -d
+openssl aes-256-cbc -k "%keyA%" -in env.bat.enc -out env.bat -d
+openssl aes-256-cbc -k "%keyA%" -in id_rsa_travis.pkk.enc -out id_rsa_travis.pkk -d
   
 set PYTHONPATH=%PYTHONPATH%;%cd%
 

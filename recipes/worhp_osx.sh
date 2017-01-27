0371262713aec56e66ade59b2ff96c6156eed85e
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-27 08:51:34 +0100

    work

--- recipes/worhp_osx.sh
+++ recipes/worhp_osx.sh
@@ -8,5 +8,5 @@ else
   try_fetch_7z libworhp_macosx10.9_gcc48_1.8-3cdb565 worhp
   export WORHP=$HOME/build/worhp
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
-  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH;$WORHP/lib
+  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$WORHP/lib
 fi

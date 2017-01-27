a53fb63346ceb43088b667f6953c9d3dc139d497
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-27 09:31:20 +0100

    work

--- fetch_common.bat
+++ fetch_common.bat
@@ -3,19 +3,10 @@ python -c "from restricted import *; download('hsl_mingw%BITNESS%_trusty.zip')"
 python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip')"
 7z x -y "slicot_mingw%BITNESS%_trusty.zip" > nul
 
-IF "%BITNESS%"=="32" (
-    python -c "from restricted import *; download('libworhp_mingw-w64_i686_1.7-dd7abd3.7z')"
-    7z x -y "libworhp_mingw-w64_i686_1.7-dd7abd3.7z" > nul
-    set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\libworhp_mingw-w64_i686_1.7-dd7abd3\bin
-)
+python -c "from restricted import *; download('libworhp_vs2013_1.9-1_16_10.7z')"
+7z x -y "libworhp_vs2013_1.9-1_16_10.7z" > nul
 
-IF "%BITNESS%"=="64" (
-    python -c "from restricted import *; download('libworhp_mingw-w64_x86_64_1.7-dd7abd3.7z')"
-    7z x -y "libworhp_mingw-w64_x86_64_1.7-dd7abd3.7z" > nul
-    set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\libworhp_mingw-w64_x86_64_1.7-dd7abd3\bin
-)
-
-
+set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\vs2013-Release\bin64
 set WORHP_LICENSE_FILE=C:\projects\binaries\testbot\restricted\worhp\unlocked.lic
 dir
 set PATH=%PATH%;C:\projects\binaries\;
--- recipes/worhp_mingw_trusty.sh
+++ recipes/worhp_mingw_trusty.sh
@@ -4,15 +4,10 @@ set -e
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  if [ "${BITNESS}" = "32" ]; then
-    try_fetch_7z libworhp_mingw-w64_i686_1.7-dd7abd3 worhp
-  fi
-  if [ "${BITNESS}" = "64" ]; then
-    try_fetch_7z libworhp_mingw-w64_x86_64_1.7-dd7abd3 worhp
-  fi
-  echo "bitness: ${BITNESS}"
+  try_fetch_7z libworhp_vs2013_1.9-1_16_10 worhp
+    echo "bitness: ${BITNESS}"
   ls $HOME/build/worhp
-  export WORHP=$HOME/build/worhp/
-  cp $WORHP/bin/* $WORHP/lib
+  export WORHP=$HOME/build/worhp/vs2013-Release/
+  mv $WORHP/bin64 $WORHP/lib
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
 fi

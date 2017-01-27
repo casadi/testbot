1816526994126370b2ca896302a38b5ef250b61b
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-27 08:27:31 +0100

    worhp

--- recipes/worhp_trusty.sh
+++ recipes/worhp_trusty.sh
@@ -7,4 +7,5 @@ else
   try_fetch_7z libworhp_ubuntu14.04_amd64_1.9-1_16_10 worhp
   export WORHP=$HOME/build/worhp
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORHP/lib
 fi

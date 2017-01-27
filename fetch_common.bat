c11f6ad7c9d7ee365b05848f815dffe924796812
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-01-27 10:48:39 +0100

    work

--- fetch_common.bat
+++ fetch_common.bat
@@ -6,7 +6,7 @@ python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip'
 python -c "from restricted import *; download('libworhp_vs2013_1.9-1_16_10.7z')"
 7z x -y "libworhp_vs2013_1.9-1_16_10.7z" > nul
 
-set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\vs2013-Release\bin64
+set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\vs2013-Release\bin%BITNESS%
 set WORHP_LICENSE_FILE=C:\projects\binaries\testbot\restricted\worhp\unlocked.lic
 dir
 set PATH=%PATH%;C:\projects\binaries\;

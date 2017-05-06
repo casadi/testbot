1a223fc902d164516b9f8edb999235712f847b3a
jgillis <joris.gillis42@gmail.com>
2017-05-06 11:52:20 +0200

    Update restricted.py

--- helpers/restricted.py
+++ helpers/restricted.py
@@ -71,7 +71,7 @@ def download(filename):
       with open(filename, 'wb') as f:
           for chunk in rs.iter_content(chunk_size=1024*1024):
               if i % 10 == 0:
-                print i, ' MB'
+                print(i, ' MB')
               i+= 1
               f.write(chunk)
   

eda4210d9a6ef26a910b6da6ea2956a7797f3c0e
jgillis <joris.gillis42@gmail.com>
2017-05-17 16:14:25 +0200

    Update mingw_octave_trusty.sh

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -9,17 +9,22 @@ if [ -z "$SETUP" ]; then
   hg clone http://hg.octave.org/mxe-octave/
   cd mxe-octave
   autoconf
-  ./configure
+  if [[ $BITNESS == 64 ]]
+  then
+    ./configure --enable-windows-64
+  else
+    ./configure --disable-windows-64
+  fi
   (while true ; do sleep 60 ; echo "ping" ; done ) &
   make mingw-w64
   ls -al
-  echo "tar -zcvf mingw_octave$SUFFIXFILE.tar.gz -C $mypwd usr /dev/null"
-  tar -zcvf mingw_octave$SUFFIXFILE.tar.gz usr >/dev/null
-  slurp_put mingw_octave$SUFFIXFILE
+  echo "tar -zcvf mingw_octave$BITNESS$SUFFIXFILE.tar.gz -C $mypwd usr /dev/null"
+  tar -zcvf mingw_octave$BITNESS$SUFFIXFILE.tar.gz usr >/dev/null
+  slurp_put mingw_octave$BITNESS$SUFFIXFILE
 else
   echo "mingw_octave_start :$BAKEVERSION:"
   pushd $HOME/build
-  fetch_tar mingw_octave trusty
+  fetch_tar mingw_octave$BITNESS mingw_octave
   popd
   echo "mingw_octave_end :$BAKEVERSION:"
   export MINGW_ROOT=$HOME/build/mingw_octave/usr

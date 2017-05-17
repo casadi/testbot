ea8203c39d7de6537d805115bea2948fbc65183c
jgillis <joris.gillis42@gmail.com>
2017-05-17 15:45:13 +0200

    Update hsl_trusty.sh

--- recipes/hsl_trusty.sh
+++ recipes/hsl_trusty.sh
@@ -9,7 +9,7 @@ if [ -z "$SETUP" ]; then
   tar -xvf coinhsl.tar.gz
   cd coinhsl-2014.01.10
   tar -xvf ../metis-4.0.3.tar.gz
-  ./configure --prefix=$mypwd/coinhsl-install LIBS="-llapack" --with-blas="-L/usr/lib -lblas" CXXFLAGS="-O2" FCFLAGS="-O2"
+  ./configure --prefix=$mypwd/coinhsl-install --disable-static --enable-shared LIBS="-llapack" --with-blas="-L/usr/lib -lblas" CXXFLAGS="-O2 -fPIC -ftls-model=local-dynamic" FCFLAGS="-O2 -fPIC -ftls-model=local-dynamic"
   make
   make install
   cd $mypwd/coinhsl-install/lib

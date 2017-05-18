db98483864915f1507dd8adee6dd94d3b4e05162
jgillis <joris.gillis42@gmail.com>
2017-05-18 12:06:38 +0200

    Update mingw_octave_trusty.sh

--- recipes/mingw_octave_trusty.sh
+++ recipes/mingw_octave_trusty.sh
@@ -28,8 +28,9 @@ else
   popd
   echo "mingw_octave_end :$BAKEVERSION:"
   export MINGW_ROOT=$HOME/build/mingw_octave$BITNESS/usr
-  export PATH=$HOME/build/mingw_octave$BITNESS/usr/bin:$HOME/build/mingw_octave$BITNESS/usr/bin/$compilerprefix:$PATH
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/build/mingw_octave$BITNESS/usr/x86_64-unknown-linux-gnu/$compilerprefix/lib/
+  export MINGW_LIB=$MINGW_ROOT/mingw_octave$BITNESS/lib
+  export PATH=$MINGW_ROOT/bin:$MINGW_ROOT/bin/$compilerprefix:$PATH
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$MINGW_ROOT/x86_64-unknown-linux-gnu/$compilerprefix/lib/:$MINGW_LIB
   mkdir -p /home/travis/build/casadi/testbot/
   # For .la files
   ln -s $HOME/build/mingw_octave$BITNESS /home/travis/build/casadi/testbot/mxe-octave

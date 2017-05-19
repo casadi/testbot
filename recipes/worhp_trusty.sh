7034ed5e3df12300a659c92b38afc0c2299aee76
jgillis <joris.gillis42@gmail.com>
2017-05-19 08:48:22 +0200

    Update worhp_trusty.sh

--- recipes/worhp_trusty.sh
+++ recipes/worhp_trusty.sh
@@ -5,7 +5,7 @@ if [ -z "$SETUP" ]; then
   echo "noop"
 else
   #try_fetch_7z libworhp_ubuntu14.04_amd64_1.9-1_16_10 worhp
-  try_fetch_zip worhp_1.10_linux.zip worhp
+  try_fetch_zip worhp_1.10_linux worhp
   export WORHP=$HOME/build/worhp
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORHP/lib

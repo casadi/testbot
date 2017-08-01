a8440952e1f98209e88a81d496fd0efe6dc2d084
jgillis <joris.gillis42@gmail.com>
2017-08-01 11:17:07 +0200

    Update extract_hversion.py

--- helpers/extract_hversion.py
+++ helpers/extract_hversion.py
@@ -1,9 +1,18 @@
-for l in open('casadi_meta.cpp','r').readlines():
-  if "CasadiMeta::version" in l:
-    version = l.split('"')[1]
-    if "+" not in version:
-      break
-  if "CasadiMeta::git_describe" in l:
-    version = l.split('"')[1]
+try:
+  for l in open('casadi_meta.cpp','r').readlines():
+    if "CasadiMeta::version" in l:
+      version = l.split('"')[1]
+      if "+" not in version:
+        break
+    if "CasadiMeta::git_describe" in l:
+      version = l.split('"')[1]
+except:
+  for l in open('casadi/config.h','r').readlines():
+    if "CASADI_VERSION_STRING" in l:
+      version = l.split('"')[1]
+      if "+" not in version:
+        break
+    if "CASADI_GIT_DESCRIBE" in l:
+      version = l.split('"')[1]
 
 print(version)

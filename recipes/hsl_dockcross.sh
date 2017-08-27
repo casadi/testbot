7797e0868afa1904b03b4828347723ef3b9e8c72
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-08-27 22:33:19 +0200

    work

--- recipes/hsl_dockcross.sh
+++ recipes/hsl_dockcross.sh
@@ -6,23 +6,32 @@ export SUFFIX=manylinux${BITNESS}_dockcross
 if [ -z "$SETUP" ]; then
   dockcross_setup_start
   dockcross_setup_finish
-  sudo apt-get install -y libblas-dev liblapack-dev
   mypwd=`pwd`
+  
+  export SUFFIXFILE=_$SUFFIX
+  export SLURP_OS=dockcross
+  export SLURP_CROSS=manylinux
+  pushd $HOME/build
+  slurp ipopt
+  popd
   pushd restricted
   wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
   tar -xvf coinhsl.tar.gz
   cd coinhsl-2014.01.10
   tar -xvf ../metis-4.0.3.tar.gz
-  dockcross bash -c "sudo yum install -y blas lapack &&./configure --prefix=$mypwd/coinhsl-install --disable-static --enable-shared LIBS='-llapack' --with-blas='-L/usr/lib -lblas' CXXFLAGS='-O2 -fPIC -ftls-model=local-dynamic' FCFLAGS='-O2 -fPIC -ftls-model=local-dynamic' && make && make install"
+  dockcross ./configure --prefix=$mypwd/coinhsl-install --disable-static --enable-shared LIBS='-L/home/travis/ipopt-install/lib' --with-blas='-lcoinblas -lcoinlapack -lcoinblas' CXXFLAGS='-O2 -fPIC -ftls-model=local-dynamic' FCFLAGS='-O2 -fPIC -ftls-model=local-dynamic' || cat config.log
+  sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
+  dockcross make
+  dockcross make install  
   cd $mypwd/coinhsl-install/lib
   ln -s libcoinhsl.so libhsl.so
   popd
-  tar -zcvf hsl_trusty.tar.gz -C $mypwd/coinhsl-install/lib . 
+  tar -zcvf hsl$SUFFIXFILE.tar.gz -C $mypwd/coinhsl-install/lib . 
 
   #echo "test" > libhsl.tar.gz
-  slurp_put hsl_trusty
+  slurp_put hsl$SUFFIXFILE
 else
-  fetch_tar hsl trusty
+  fetch_tar hsl $SUFFIX
   rm -f hsl/*.a
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/build/hsl
   export HSL=$HOME/build/hsl

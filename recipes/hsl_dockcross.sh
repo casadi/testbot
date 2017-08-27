da7f33a7eec50a1505b846df64faf01dc7079613
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-08-27 21:35:31 +0200

    work

---
+++ recipes/hsl_dockcross.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+export SUFFIX=manylinux${BITNESS}_dockcross
+
+if [ -z "$SETUP" ]; then
+  dockcross_setup_start
+  dockcross_setup_finish
+  sudo apt-get install -y libblas-dev liblapack-dev
+  mypwd=`pwd`
+  pushd restricted
+  wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
+  tar -xvf coinhsl.tar.gz
+  cd coinhsl-2014.01.10
+  tar -xvf ../metis-4.0.3.tar.gz
+  dockcross bash -c "yum install -y blas lapack &&./configure --prefix=$mypwd/coinhsl-install --disable-static --enable-shared LIBS='-llapack' --with-blas='-L/usr/lib -lblas' CXXFLAGS='-O2 -fPIC -ftls-model=local-dynamic' FCFLAGS='-O2 -fPIC -ftls-model=local-dynamic' && make && make install"
+  cd $mypwd/coinhsl-install/lib
+  ln -s libcoinhsl.so libhsl.so
+  popd
+  tar -zcvf hsl_trusty.tar.gz -C $mypwd/coinhsl-install/lib . 
+
+  #echo "test" > libhsl.tar.gz
+  slurp_put hsl_trusty
+else
+  fetch_tar hsl trusty
+  rm -f hsl/*.a
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/build/hsl
+  export HSL=$HOME/build/hsl
+  export casadi_build_flags="$casadi_build_flags -DWITH_HSL=ON"  
+fi

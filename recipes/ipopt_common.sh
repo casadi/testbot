61eb7071acff03bb6b84b2eeadec872bb36e1feb
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-08-27 18:16:56 +0200

    work

--- recipes/ipopt_common.sh
+++ recipes/ipopt_common.sh
@@ -35,6 +35,9 @@ EOF
   popd
   mkdir build
   pushd build
+  buildenv echo 123
+  dockcross echo 123
+  dockcross ../configure $FLAGS --prefix=$HOME/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl  
   buildenv ../configure $FLAGS --prefix=$HOME/ipopt-install --disable-shared ADD_FFLAGS=-fPIC ADD_CFLAGS=-fPIC ADD_CXXFLAGS=-fPIC --with-blas=BUILD --with-lapack=BUILD --with-mumps=BUILD --with-metis=BUILD --without-hsl --without-asl
   buildenv make
   buildenv make install

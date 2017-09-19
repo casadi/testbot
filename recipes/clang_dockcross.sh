a62bc0050dff33d22d3c19778d47de4f183ba84b
jgillis <joris.gillis42@gmail.com>
2017-09-19 18:33:13 +0200

    Update clang_dockcross.sh

--- recipes/clang_dockcross.sh
+++ recipes/clang_dockcross.sh
@@ -13,17 +13,17 @@ if [ -z "$SETUP" ]; then
 
   #sudo apt-get install libc6-dev
 
-  svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$VERSION/final/ llvm
+  svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_$VERSION/final/ llvm >/dev/null
   cd llvm/tools
-  svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$VERSION/final/ clang
+  svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_$VERSION/final/ clang >/dev/null
   cd ../projects
-  svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/ libcxx
+  svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_$VERSION/final/ libcxx >/dev/null
   cd libcxx && patch -p0 -i $mypwd/recipes/libcxx.patch && cd ..
   cd ../..
   mkdir build
   cd build
 
-  export PATH=/opt/python/cp27-cp27m/bin:\$PATH >> $HOME/path_appends.txt
+  echo 'export PATH=/opt/python/cp27-cp27m/bin:$PATH' >> $HOME/path_appends.txt
   build_env cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$mypwd/install" ../llvm
   build_env make clang-tblgen install -j2
   cp bin/clang-tblgen "$mypwd/install/bin"

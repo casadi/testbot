ebe9d795604be15b767918a6928b37cf1a6daa1b
jgillis <joris.gillis42@gmail.com>
2017-09-24 13:30:29 +0200

    Update slicot_dockcross.sh

--- recipes/slicot_dockcross.sh
+++ recipes/slicot_dockcross.sh
@@ -6,5 +6,6 @@ if [ -z "$SETUP" ]; then
   echo "noop"
 else
   sudo apt-get install -y libslicot-dev
+  export SLICOT_LIBRARY_DIR=/host/usr/lib/x86_64-linux-gnu/
   export casadi_build_flags="$casadi_build_flags -DWITH_SLICOT=ON"
 fi

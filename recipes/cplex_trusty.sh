c0c179b2d2b83086c73daea278e43f6160fb4d9b
jgillis <joris.gillis42@gmail.com>
2017-10-13 23:11:16 +0200

    Update cplex_trusty.sh

--- recipes/cplex_trusty.sh
+++ recipes/cplex_trusty.sh
@@ -5,7 +5,7 @@ if [ -z "$SETUP" ]; then
   echo "noop"
 else
   echo "nothing to do"
-  try_fetch_tar cplex_linux1263
+  try_fetch_tar cplex_linux1263 cplex
   export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
   export CPLEX=$HOME/build/cplex/
   export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1263"  

f3a4af2d9602ae98991b015a4cf0701b0371f590
jgillis <joris.gillis42@gmail.com>
2017-11-01 17:38:58 +0100

    Update worhp_mingw_trusty.sh

--- recipes/worhp_mingw_trusty.sh
+++ recipes/worhp_mingw_trusty.sh
@@ -8,7 +8,7 @@ else
   #try_fetch_zip worhp_1.10_mingw worhp
     echo "bitness: ${BITNESS}"
   ls $HOME/build/worhp
-  export WORHP=$HOME/build/worhp/vs2013-Release
+  export WORHP=$HOME/build/worhp
   mv $WORHP/bin${BITNESS} $WORHP/lib
   export WORHP_LICENSE_FILE=$HOME/build/testbot/restricted/worhp/unlocked.lic
   export casadi_build_flags="$casadi_build_flags -DWITH_WORHP=ON"

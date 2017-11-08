9cf5c8c6b60308751b7fe1af37494f059d826e60
jgillis <joris.gillis42@gmail.com>
2017-11-08 09:30:50 +0100

    Update fetch_common.bat

--- fetch_common.bat
+++ fetch_common.bat
@@ -3,8 +3,8 @@ python -c "from restricted import *; download('hsl_mingw%BITNESS%_trusty.zip')"
 python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip')"
 7z x -y "slicot_mingw%BITNESS%_trusty.zip" > nul
 
-python -c "from restricted import *; download('libworhp_vs2013_1.9-1_16_10.7z')"
-7z x -y "libworhp_vs2013_1.9-1_16_10.7z" > nul
+python -c "from restricted import *; download('libworhp_vs2013_1.10-2.7z')"
+7z x -y "libworhp_vs2013_1.10-2.7z" > nul
 
 set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\vs2013-Release\bin%BITNESS%
 set WORHP_LICENSE_FILE=C:\projects\binaries\testbot\restricted\worhp\unlocked.lic

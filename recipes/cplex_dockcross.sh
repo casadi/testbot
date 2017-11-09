3743e57135c0c743b5a37ab3771ad613478d9914
jgillis <joris.gillis42@gmail.com>
2017-11-09 10:42:33 +0100

    Update cplex_dockcross.sh

--- recipes/cplex_dockcross.sh
+++ recipes/cplex_dockcross.sh
@@ -4,11 +4,10 @@ set -e
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  echo "nothing to do"
-  #try_fetch_tar cplex12.4 cplex
-  #try_fetch_tar cplex12.4-concert cplex-concert
-  #export CMAKE_INCLUDE_PATH=$CMAKE_INCLUDE_PATH:$HOME/build/cplex/include:$HOME/build/cplex-concert/include
-  #export CMAKE_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:$HOME/build/cplex/lib/x86-64_sles10_4.1/static_pic/:$HOME/build/cplex-concert/lib/x86-64_sles10_4.1/static_pic/
-  #export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
-  #export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON"  
+  try_fetch_tar cplex_linux1263 cplex
+  export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
+  export CPLEX=$HOME/build/cplex/
+  ls $CPLEX
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86-64_linux
+  export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1263"  
 fi

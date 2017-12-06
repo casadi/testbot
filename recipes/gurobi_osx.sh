df45d16ea3d272f6442afb071856fb6475dc863e
jgillis <joris.gillis42@gmail.com>
2017-12-06 20:17:17 +0100

    Update gurobi_osx.sh

--- recipes/gurobi_osx.sh
+++ recipes/gurobi_osx.sh
@@ -6,6 +6,7 @@ if [ -z "$SETUP" ]; then
 else
   fetch_zip gurobi650 osx
   export GUROBI_HOME=/Users/travis/build/gurobi650
+  sudo install_name_tool -id "@rpath/libgurobi65.so" $GUROBI_HOME/lib/libgurobi65.so
   export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$GUROBI_HOME/lib
   export casadi_build_flags="$casadi_build_flags -DWITH_GUROBI=ON"  
 fi

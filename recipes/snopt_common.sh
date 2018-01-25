d171d565d759980191fa16b2d481befa86041fdd
Joris Gillis <joris.gillis42gmail.com>
2018-01-25 15:49:01 +0100

    work

--- recipes/snopt_common.sh
+++ recipes/snopt_common.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [ -z "$SETUP" ]; then
-
-else
-  fetch_tar ipopt $SUFFIX
-  export SNOPT=$HOME/build/snopt
-  export casadi_build_flags="$casadi_build_flags -DWITH_SNOPT=ON"  
-fi
+fetch_tar ipopt $SUFFIX
+export SNOPT=$HOME/build/snopt
+export casadi_build_flags="$casadi_build_flags -DWITH_SNOPT=ON"
 
 export SNOPT_LICENSE=$HOME/build/testbot/restricted/snopt7.lic

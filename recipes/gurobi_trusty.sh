30282ffbc4fa65267472bdb775f34c7ff846ba56
Joris Gillis <joris.gillis42gmail.com>
2018-01-26 12:04:14 +0100

    Dwork

--- recipes/gurobi_trusty.sh
+++ recipes/gurobi_trusty.sh
@@ -8,5 +8,7 @@ else
   export GUROBI_HOME=/home/travis/build/gurobi650
   export GRB_LICENSE_FILE=$HOME/build/testbot/restricted/gurobi.lic
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GUROBI_HOME/lib
+  ls $GUROBI_HOME/lib
+  echo $LD_LIBRARY_PATH
   export casadi_build_flags="$casadi_build_flags -DWITH_GUROBI=ON"  
 fi

912d378bb0257410df28ca32647f69007b6a1c52
jgillis <joris.gillis42@gmail.com>
2018-01-27 23:19:47 +0100

    Update bonmin_mingw_trusty.sh

--- recipes/bonmin_mingw_trusty.sh
+++ recipes/bonmin_mingw_trusty.sh
@@ -8,6 +8,8 @@ if [ -z "$SETUP" ]; then
   export SUFFIXFILE=_$SUFFIX
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
   export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32"
+else
+  export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:$HOME/bonmin-install/lib/pkgconfig
 fi
 
 source $RECIPES_FOLDER/bonmin_common.sh

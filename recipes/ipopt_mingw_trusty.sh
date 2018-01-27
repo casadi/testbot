f60f97ad9d577735f0b8623dff37132dfa56962f
jgillis <joris.gillis42@gmail.com>
2018-01-27 23:19:24 +0100

    Update ipopt_mingw_trusty.sh

--- recipes/ipopt_mingw_trusty.sh
+++ recipes/ipopt_mingw_trusty.sh
@@ -7,6 +7,8 @@ if [ -z "$SETUP" ]; then
   export SUFFIXFILE=_$SUFFIX
   # build must contain mingw, in order for the hsl loader to look for .dll as opposed to .so
   export FLAGS="--host $compilerprefix --enable-dependency-linking --build mingw32"
+else
+  export PKG_CONFIG_LIBDIR=$PKG_CONFIG_LIBDIR:$HOME/ipopt-install/lib/pkgconfig
 fi
 source $RECIPES_FOLDER/ipopt_common.sh
 

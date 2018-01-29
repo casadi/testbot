018d4c5436c003d761d4bc8d3ebcceb7bcd2f8b1
jgillis <joris.gillis42@gmail.com>
2018-01-29 14:53:00 +0100

    Update cplex_mingw_trusty.sh

--- recipes/cplex_mingw_trusty.sh
+++ recipes/cplex_mingw_trusty.sh
@@ -9,6 +9,6 @@ else
   export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
   export CPLEX=$HOME/build/cplex/
   ls $CPLEX
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86-64_win64
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86_win64
   export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1280"  
 fi

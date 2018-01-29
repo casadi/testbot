e687a3a57e7d191e6d74adb41fe52e7a5830f420
jgillis <joris.gillis42@gmail.com>
2018-01-29 14:51:40 +0100

    Create cplex_mingw_trusty.sh

---
+++ recipes/cplex_mingw_trusty.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#set -e
+
+if [ -z "$SETUP" ]; then
+  echo "noop"
+else
+  echo "nothing to do"
+  try_fetch_tar cplex_win1280 cplex
+  export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
+  export CPLEX=$HOME/build/cplex/
+  ls $CPLEX
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86-64_win64
+  export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1280"  
+fi

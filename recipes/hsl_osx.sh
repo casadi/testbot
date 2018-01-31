2c6a43d31ad76f1bc0a612a3b748e58fd005b0b9
jgillis <joris.gillis42@gmail.com>
2018-01-31 17:56:37 +0100

    Update hsl_osx.sh

--- recipes/hsl_osx.sh
+++ recipes/hsl_osx.sh
@@ -22,7 +22,7 @@ if [ -z "$SETUP" ]; then
   ls
   mv libcoinhsl.0.dylib libhsl.dylib
   rm libcoinhsl.dylib
-  #sudo install_name_tool -change $GCC_FULL "@rpath/libgcc_s.1.dylib" libhsl.dylib
+  sudo install_name_tool -change $GCC_FULL_ALT "@rpath/libgcc_s.1.dylib" libhsl.dylib
   sudo install_name_tool -id "@rpath/libhsl.dylib" libhsl.dylib
   otool -l libhsl.dylib
   popd

7cfe467a601b6d4547dcd16f0ad6bdf8d4bdc23e
jgillis <joris.gillis42@gmail.com>
2018-02-05 16:16:33 +0100

    Update hsl_mingw_trusty.sh

--- recipes/hsl_mingw_trusty.sh
+++ recipes/hsl_mingw_trusty.sh
@@ -16,7 +16,7 @@ if [ -z "$SETUP" ]; then
   pushd restricted
   wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
   tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz
-  #sed -i "s/AC_INIT(coinhsl/AC_INIT(hsl/" configure.ac
+  sed -i "s/AC_INIT(coinhsl/AC_INIT(hsl/" configure.ac
   autoconf
   ./configure --disable-static --enable-shared --host $compilerprefix --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="" FCFLAGS="-O2" CFLAGS="-O2" || cat config.log
   sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
@@ -27,7 +27,7 @@ if [ -z "$SETUP" ]; then
 
   mkdir $mypwd/pack
   cd $mypwd/coinhsl-install/bin
-  cp libcoinhsl-0.dll $mypwd/pack/libhsl.dll
+  cp libhsl-0.dll $mypwd/pack/libhsl.dll
   pushd $mypwd/pack/
   gendef libhsl.dll - | tee  libhsl.def
   $compilerprefix-dlltool --dllname libhsl.dll -d libhsl.def  -l libhsl.lib

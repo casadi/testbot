d39548652395408c74ad49b64dcd3cf56dda3ca7
jgillis <joris.gillis42@gmail.com>
2018-02-05 08:44:51 +0100

    Update hsl_mingw_trusty.sh

--- recipes/hsl_mingw_trusty.sh
+++ recipes/hsl_mingw_trusty.sh
@@ -16,11 +16,11 @@ if [ -z "$SETUP" ]; then
   pushd restricted
   wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
   tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz
+  sed -i "s/AC_INIT(coinhsl/AC_INIT(hsl/" configure.ac
+  autoconf
   ./configure --disable-static --enable-shared --host $compilerprefix --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="" FCFLAGS="-O2" CFLAGS="-O2" || cat config.log
   sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
   sed -i "s/soname_spec=.*/soname_spec=\"libhsl.dll\"/" libtool
-  sed -i "s/libname_spec=.*/libname_spec=\"libhsl\"/" libtool
-  sed -i "s/library_names_spec=.*/libname_spec=\"libhsl.dll.a\"/" libtool
 
   make
   make install

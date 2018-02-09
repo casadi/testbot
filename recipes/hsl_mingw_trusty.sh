4384c06ae8b20cf7e8b8a19e12d9345e01eea313
jgillis <joris.gillis42@gmail.com>
2018-02-09 10:18:08 +0100

    Update hsl_mingw_trusty.sh

--- recipes/hsl_mingw_trusty.sh
+++ recipes/hsl_mingw_trusty.sh
@@ -16,17 +16,20 @@ if [ -z "$SETUP" ]; then
   pushd restricted
   wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/OLD/metis-4.0.3.tar.gz
   tar -xvf coinhsl.tar.gz && cd coinhsl-2014.01.10 && tar -xvf ../metis-4.0.3.tar.gz
-  sed -i "s/AC_INIT(coinhsl/AC_INIT(hsl/" configure.ac
+  sed -i "s/coinhsl/hsl/" configure.ac
   sed -i "s/coinhsl/hsl/g" Makefile.am
   mv libcoinhsl.sym libhsl.sym
+  mv coinhsl.pc.in hsl.pc.in
+  sed -i "s/coinhsl/hsl/g" hsl.pc.in
   aclocal
   autoheader
   automake --add-missing
   automake
   autoconf
+
   ./configure --disable-static --enable-shared --host $compilerprefix --prefix=$mypwd/coinhsl-install LIBS="-L/home/travis/ipopt-install/lib" --with-blas="-lcoinblas -lcoinlapack -lcoinblas" CXXFLAGS="" FCFLAGS="-O2" CFLAGS="-O2" || cat config.log
+  sed -i "s/soname_spec=.*/soname_spec=\"libhsl.dll\"/" libtool
   sed -i "s/deplibs_check_method=.*/deplibs_check_method=\"pass_all\"/" libtool
-  #sed -i "s/soname_spec=.*/soname_spec=\"libhsl.dll\"/" libtool
 
   make
   make install
@@ -36,8 +39,8 @@ if [ -z "$SETUP" ]; then
   #cp libhsl-0.dll $mypwd/pack/libhsl.dll
   cp *.dll $mypwd/pack/
   pushd $mypwd/pack/
-  #gendef libhsl.dll - | tee  libhsl.def
-  #$compilerprefix-dlltool --dllname libhsl.dll -d libhsl.def  -l libhsl.lib
+  gendef libhsl.dll - | tee  libhsl.def
+  $compilerprefix-dlltool --dllname libhsl.dll -d libhsl.def  -l libhsl.lib
   popd
   cp /usr/lib/gcc/$compilerprefix/5.3-posix/*.dll $mypwd/pack
   cp /usr/$compilerprefix/lib/*.dll $mypwd/pack

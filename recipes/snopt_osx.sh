9af5a37ce4d4876fa008cb370a8d1c5f1acfa99f
jgillis <joris.gillis42@gmail.com>
2018-02-10 21:43:47 +0100

    Update snopt_osx.sh

--- recipes/snopt_osx.sh
+++ recipes/snopt_osx.sh
@@ -2,6 +2,6 @@
 export SUFFIX=osx
 source $RECIPES_FOLDER/snopt_common.sh
 export MY_LIBRARY_PATH=$SNOPT:$MY_LIBRARY_PATH
-sudo install_name_tool -change /opt/local/lib/libgcc/libgfortran.3.dylib $FORTRAN_FULL $SNOPT/*.dylib
-sudo install_name_tool -change /opt/local/lib/libgcc/libgcc_s.1.dylib $GCC_FULL $SNOPT/*.dylib
-sudo install_name_tool -change /opt/local/lib/libgcc/libquadmath.0.dylib $QUADMATH_FULL $SNOPT/*.dylib
+sudo install_name_tool -change /opt/local/lib/libgcc/libgfortran.3.dylib "@rpath/libgfortran.3.dylib" $SNOPT/libsnopt7.dylib
+sudo install_name_tool -change /opt/local/lib/libgcc/libgcc_s.1.dylib "@rpath/libgcc_s.1.dylib" $SNOPT/libsnopt7.dylib
+sudo install_name_tool -change /opt/local/lib/libgcc/libquadmath.0.dylib "@rpath/libquadmath.0.dylib" $SNOPT/libsnopt7.dylib

34d8ae3441a06fa95ebcb3dd111e220b9727b796
jgillis <joris.gillis42@gmail.com>
2018-02-11 13:12:24 +0100

    Update fetch_common.bat

--- fetch_common.bat
+++ fetch_common.bat
@@ -6,7 +6,10 @@ python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip'
 python -c "from restricted import *; download('libworhp_vs2013_1.10-2.7z')"
 7z x -y "libworhp_vs2013_1.10-2.7z" > nul
 
-set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\bin%BITNESS%
+python -c "from restricted import *; download('gurobi650_mingw%BITNESS%_trusty.zip')"
+7z x -y "gurobi650_mingw64_trusty.zip" > nul
+
+set PATH=%PATH%;C:\projects\binaries\;C:\projects\binaries\bin%BITNESS%;C:\projects\binaries\bin
 set WORHP_LICENSE_FILE=C:\projects\binaries\testbot\restricted\worhp\unlocked.lic
 dir
 set PATH=%PATH%;C:\projects\binaries\;

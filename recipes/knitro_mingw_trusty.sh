a69c0a456693dd0ebc770fbef61b8fe4261456b5
jgillis <joris.gillis42@gmail.com>
2018-02-12 11:42:59 +0100

    Update knitro_mingw_trusty.sh

--- recipes/knitro_mingw_trusty.sh
+++ recipes/knitro_mingw_trusty.sh
@@ -2,5 +2,5 @@
 
 export SUFFIX=mingw${BITNESS}_trusty
 source $RECIPES_FOLDER/knitro_common.sh
-mv knitro-10.3.2-z-WinMSVC10-${BITNESS}/* .
+mv knitro/knitro-10.3.2-z-WinMSVC10-${BITNESS}/* knitro
 

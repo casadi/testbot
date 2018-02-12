2fee51dfbd9676337323751cf54868194b963e9e
jgillis <joris.gillis42@gmail.com>
2018-02-12 11:45:03 +0100

    Update knitro_osx.sh

--- recipes/knitro_osx.sh
+++ recipes/knitro_osx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export SUFFIX=osx
 source $RECIPES_FOLDER/knitro_common.sh
-mv knitro-10.3.1-z-MacOS-64/* .
+mv knitro/knitro-10.3.1-z-MacOS-64/* knitro
 export MY_LIBRARY_PATH=$KNITRO:$MY_LIBRARY_PATH

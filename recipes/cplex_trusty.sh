23420bcd0f77c6a082d115bbf407d5c9e8a30959
jgillis <joris.gillis42@gmail.com>
2018-02-12 14:08:56 +0100

    Update cplex_trusty.sh

--- recipes/cplex_trusty.sh
+++ recipes/cplex_trusty.sh
@@ -7,7 +7,7 @@ else
   echo "nothing to do"
   try_fetch_tar cplex_linux1263 cplex
   export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
-  export CPLEX=$HOME/build/
+  export CPLEX=$HOME/build/cplex
   ls $CPLEX
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86-64_linux
   export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1263"  

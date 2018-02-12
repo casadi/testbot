04a0d3c9f34700e6cf62246c873c5cdd31e815b2
jgillis <joris.gillis42@gmail.com>
2018-02-12 12:55:05 +0100

    Update knitro_trusty.sh

--- recipes/knitro_trusty.sh
+++ recipes/knitro_trusty.sh
@@ -2,7 +2,8 @@
 
 export SUFFIX=trusty
 source $RECIPES_FOLDER/knitro_common.sh
-ls
-ls knitro
+pushd $KNITRO
+find . -name "*.a" | xargs rm
+popd
 mv knitro/knitro-10.3.0-z-Linux-64/* knitro
 export LD_LIBRARY_PATH=$KNITRO:$LD_LIBRARY_PATH

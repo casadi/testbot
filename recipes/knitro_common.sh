a4c0b21be70cadcd6d64d79098a3b96b77fbaef9
jgillis <joris.gillis42@gmail.com>
2018-02-12 11:44:28 +0100

    Update knitro_common.sh

--- recipes/knitro_common.sh
+++ recipes/knitro_common.sh
@@ -3,7 +3,7 @@
 fetch knitro $SUFFIX
 export KNITRO=$HOME/build/knitro
 pushd $KNITRO
-find . -name "*.a" | xargs rm
+find . -name "*.a" | xargs rm || allowfail 1
 popd
 export casadi_build_flags="$casadi_build_flags -DWITH_KNITRO=ON"
 

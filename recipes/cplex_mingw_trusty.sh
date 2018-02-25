f6ed3bb9d0124a3e1da10ad599c0ce1157a4823e
jgillis <joris.gillis42@gmail.com>
2018-02-25 21:02:31 +0100

    Update cplex_mingw_trusty.sh

--- recipes/cplex_mingw_trusty.sh
+++ recipes/cplex_mingw_trusty.sh
@@ -4,11 +4,15 @@
 if [ -z "$SETUP" ]; then
   echo "noop"
 else
-  echo "nothing to do"
-  try_fetch_zip cplex_win1280 cplex
-  export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
-  export CPLEX=$HOME/build/
-  ls $CPLEX
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86_win64
-  export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1280"  
+  if [[ $BITNESS == *64* ]]
+  then
+    echo "nothing to do"
+    try_fetch_zip cplex_win1280 cplex
+    export ILOG_LICENSE_FILE=$ILOG_LICENSE_FILE=$HOME/build/testbot/restricted/cplex/access.ilm
+    export CPLEX=$HOME/build/
+    ls $CPLEX
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CPLEX/cplex/bin/x86_win64
+    export CXXFLAGS="$CXXFLAGS -DCPXSIZE_BITS=64"
+    export casadi_build_flags="$casadi_build_flags -DWITH_CPLEX=ON -DWITH_CPLEX_SHARED=ON -DCPLEX_VERSION=1280"
+  fi
 fi

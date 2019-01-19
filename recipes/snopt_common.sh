a1e250238118e46aefa151be6ad79df73b13cb9d
jgillis <joris.gillis42@gmail.com>
2019-01-19 13:48:22 +0100

    Update snopt_common.sh

--- recipes/snopt_common.sh
+++ recipes/snopt_common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fetch snopt77 $SUFFIX
-export SNOPT=$HOME/build/snopt
+export SNOPT=$HOME/build/snopt77
 export casadi_build_flags="$casadi_build_flags -DWITH_SNOPT=ON"
 
 export SNOPT_LICENSE=$HOME/build/testbot/restricted/snopt7.lic

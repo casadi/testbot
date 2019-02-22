b9a71826cdf16306ed19ee96fe75b234ab205629
jgillis <joris.gillis42@gmail.com>
2019-02-22 08:20:22 +0100

    Update bonmin_common.sh

--- recipes/bonmin_common.sh
+++ recipes/bonmin_common.sh
@@ -52,7 +52,7 @@ EOF
   slurp_put bonmin$SUFFIXFILE
 else
   fetch_tar bonmin $SUFFIX
-  export casadi_build_flags="$casadi_build_flags -DWITH_BONMIN=ON -DWITH_CLP=ON"
+  export casadi_build_flags="$casadi_build_flags -DWITH_BONMIN=ON -DWITH_CLP=ON -DWITH_CBC=ON"
   
   export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$HOME/bonmin-install/lib/pkgconfig
   pushd $HOME/ && ln -s  $HOME/build/bonmin bonmin-install && popd

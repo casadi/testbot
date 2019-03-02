56cc62c792303bdb8323f6f19350ef03310f9b45
jgillis <joris.gillis42@gmail.com>
2019-03-02 20:59:58 +0100

    Update gurobi_osx.sh

--- recipes/gurobi_osx.sh
+++ recipes/gurobi_osx.sh
@@ -8,5 +8,6 @@ else
   export GUROBI_HOME=/Users/travis/build/gurobi650
   sudo install_name_tool -id "@rpath/libgurobi65.so" $GUROBI_HOME/lib/libgurobi65.so
   export MY_LIBRARY_PATH=$MY_LIBRARY_PATH:$GUROBI_HOME/lib
-  export casadi_build_flags="$casadi_build_flags -DWITH_GUROBI=ON"  
+  export casadi_build_flags="$casadi_build_flags -DWITH_GUROBI=ON"
+  export SKIP_GUROBI_TESTS=1
 fi

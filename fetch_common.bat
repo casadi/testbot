4ff115873475b9ef1a7afb6454693a4b1d8d452f
jgillis <joris.gillis42@gmail.com>
2019-09-30 23:00:13 +0200

    Update fetch_common.bat

--- fetch_common.bat
+++ fetch_common.bat
@@ -4,7 +4,7 @@ python -c "from restricted import *; download('slicot_mingw%BITNESS%_trusty.zip'
 7z x -y "slicot_mingw%BITNESS%_trusty.zip" > nul
 
 python -c "from restricted import *; download('worhp_1.12-1_win%BITNESS%.zip')"
-7z x -y "worhp_1.12-1_win%BITNESS%.zip" -oworhp > nul
+7z x -y "worhp_1.13-0_win%BITNESS%.zip" -oworhp > nul
 
 python -c "from restricted import *; download('gurobi650_mingw%BITNESS%_trusty.zip')"
 7z x -y "gurobi650_mingw%BITNESS%_trusty.zip" > nul

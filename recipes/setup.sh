97c7530402d32168c60e157dbb50002cf0e2ca3a
jgillis <joris.gillis42@gmail.com>
2020-09-02 17:50:26 +0200

    Update setup.sh

--- recipes/setup.sh
+++ recipes/setup.sh
@@ -20,12 +20,15 @@ pip install psutil
 echo $?
 pip install pyaml
 openssl version -a
+
+set +e +E
 openssl aes-256-cbc -k "$keypass" -in id_rsa_travis.enc -out id_rsa_travis -d || openssl aes-256-cbc -iter 100000 -pbkdf2 -k "$keypass" -in id_rsa_travis.enc111 -out id_rsa_travis -d
 echo $?
 openssl aes-256-cbc -k "$keypass" -in testbotcredentials.py.enc -out testbotcredentials.py -d || openssl aes-256-cbc -iter 100000 -pbkdf2 -k "$keypass" -in testbotcredentials.py.enc111 -out testbotcredentials.py -d
 echo $?
 openssl aes-256-cbc -k "$keypass" -in env.sh.enc -out env.sh -d || echo $? && openssl aes-256-cbc -iter 100000 -pbkdf2 -k "$keypass" -in env.sh.enc111 -out env.sh -d
 echo $?
+set -e -E
 
 chmod 600 id_rsa_travis
 

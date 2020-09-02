f315542f2dac2dc18b707d40d535ee54c0bfd724
jgillis <joris.gillis42@gmail.com>
2020-09-02 16:04:27 +0200

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -3,10 +3,16 @@
 (while true ; do sleep 60 ; echo "ping" ; done ) &
 shell_session_update() { :; } # Workaround for travis-ci/travis-ci#6522
 pushd casadi && export COMMIT=`git rev-parse --short=7 HEAD` && popd
+echo $?
+echo "COMMIT: $COMMIT"
 pushd ../../ && git clone https://github.com/casadi/testbot.git
+echo $?
 pushd testbot && source recipes/setup.sh && popd
+echo $?
 
 ssh-keyscan web.sourceforge.net >> ~/.ssh/known_hosts 
+echo $?
 
 echo "before_install.sh finish"
 pwd
+echo $?

e56f14b84bc5fd80f40cd97fcbe41316978c0aa8
jgillis <joris.gillis42@gmail.com>
2020-12-15 12:38:16 +0100

    Update lapack_osx.sh

--- recipes/lapack_osx.sh
+++ recipes/lapack_osx.sh
@@ -25,11 +25,11 @@ else
   fetch_tar lapack $SUFFIX
   export LIB=$HOME/build/lapack
   export DYLD_LIBRARY_PATH=$LIB:$DYLD_LIBRARY_PATH
-  cp lapack/libblas.a lapack/libblas.lib
-  cp lapack/liblapack.a lapack/liblapack.lib
   echo "lapack"
   pwd
   ls
   ls $LIB
+  cp lapack/libblas.a lapack/libblas.lib
+  cp lapack/liblapack.a lapack/liblapack.lib
   export casadi_build_flags="$casadi_build_flags -DWITH_LAPACK=ON -DBLA_VENDOR=Generic -DBLA_STATIC=ON"  
 fi

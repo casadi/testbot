b7d0ba6e6869093e7a6fc4abc62cfd517cc0a4fb
jgillis <joris.gillis42@gmail.com>
2020-12-18 15:39:49 +0100

    Update openblas_common.sh

--- recipes/openblas_common.sh
+++ recipes/openblas_common.sh
@@ -10,7 +10,7 @@ if [ -z "$SETUP" ]; then
   mkdir $HOME/openblas-install
   #build_env cmake -DDYNAMIC_ARCH=ON -DCMAKE_INSTALL_PREFIX=$HOME/openblas-install ..
   # DYNAMIC_OLDER=1 
-  build_env make DYNAMIC_ARCH=1 NO_SHARED=0 USE_OPENMP=0 USE_THREAD=1 NUM_THREADS=32 > /dev/null
+  build_env make DYNAMIC_ARCH=1 NO_SHARED=0 USE_OPENMP=0 USE_THREAD=1 NUM_THREADS=32 -j2 > /dev/null
   build_env make PREFIX=$HOME/openblas-install install
   install_name_tool -id "@rpath/libopenblas.0.dylib" $HOME/openblas-install/lib/libopenblas.0.dylib
   popd
